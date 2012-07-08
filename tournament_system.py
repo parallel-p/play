645f78aaf1c58764b9537ed4f3bc4eb0ca0f6cc4
Ivan Uvarov <totosus@gmail.com>
2012-07-08 11:29:43 +0400

    modified:   tournament_system.py

--- tournament_system.py
+++ tournament_system.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-class TournamentSystem(self):
-    def play_tournament():
+class TournamentSystem():
+    def play_tournament(self):
         players=__load_players__()
-        if self.tournament_type==1 :#на выбывание
+        if self.tournament_type==1 :#knockout
             while len(players)>1:
                 this_tour=Tour(players,tournament_type=1)
                 players=this_tour.play()
