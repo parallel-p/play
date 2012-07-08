52039d945a66755131647cec1e0e99b07ae6b58d
Ivan Uvarov <totosus@gmail.com>
2012-07-08 13:02:38 +0400

    Revert "Squashed commit of the following:"
    This reverts commit 84b0a52ee93b87531756855aea7def27cb6c94cb.

--- player.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from hashlib import md5
-
-class Player:
-    def __init__(self,filename,author_name,bot_name):
-        self.name=bot_name
-        self.author_name=author_name
-        self.file_name=filename
-        self.player_id=md5((bot_name+author_name).encode('utf8')).hexdigest()
