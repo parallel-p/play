58e113ad3dc0a1182bdf40e239b5df25110795e8
Timofey Kukushkin <reebokey@me.com>
2012-07-11 13:36:53 +0400

    rm game_supporting_classes.py

--- tournament_system/game_supporting_classes.py
+++
@@ -1,8 +0,0 @@
-class GameSignature:
-
-    def __init__(self, tournament_id=None, round_id=None, series_id=None,
-                 game_id=None):
-        self.round_id = round_id
-        self.series_id = series_id
-        self.game_id = game_id
-        self.tournament_id = tournament_id
