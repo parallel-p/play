e7ebd4c88c0db4f33da4c2bab84bf2d525d99beb
Timofey Kukushkin <reebokey@me.com>
2012-07-11 15:40:01 +0400

    Fixes

--- tournament_system/tournament_system.py
+++ tournament_system/tournament_system.py
@@ -3,9 +3,8 @@ from exceptions import NoResultsException
 
 
 class Series:
-
     '''
-    Series - a collection games of one round of  involving the same members.
+    Series - a collection games of one round of involving the same members.
     '''
 
     def __init__(self, initial_jurystates_list, signature, config,
@@ -23,16 +22,15 @@ class Series:
         '''
         Starts all games in series.
         '''
-        results = {}
+        self._results = {}
         for game_id, initial_jurystate in enumerate(
                 self._initial_jurystates_list):
             self._signature.game_id = game_id
             game = Game(initial_jurystate, self._signature, self._config,
-                              self._players_list)
+                        self._players_list)
             game.run_engine()
             points = game.get_results()
-            results[self._signature] = points
-        self._results = results
+            self._results[self._signature] = points
 
     def get_results(self):
         '''
