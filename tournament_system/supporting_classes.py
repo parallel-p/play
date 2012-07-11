1da2b0ed56e9bb1e77e63c08272b7c14358753ef
Timofey Kukushkin <reebokey@me.com>
2012-07-11 15:04:47 +0400

    Add comparison operation to GameSignature

--- tournament_system/supporting_classes.py
+++ tournament_system/supporting_classes.py
@@ -6,3 +6,15 @@ class GameSignature:
         self.series_id = series_id
         self.game_id = game_id
         self.tournament_id = tournament_id
+
+    def __lt__(self, signature):
+        if self.tournament_id == signature.tournament_id:
+            if self.series_id == signature.series_id:
+                if self.game_id == signature.game_id:
+                    return False
+                else:
+                    return self.game_id < signature.game_id
+            else:
+                return self.series_id < signature.series_id
+        else:
+            return self.tournament_id < signature.tournament_id
