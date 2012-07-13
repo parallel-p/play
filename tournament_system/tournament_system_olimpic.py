985980dbb6b0974ab2d4bf5020e941bd89632784
Vadim Drobinin <vadim@drobinin.com>
2012-07-13 12:00:31 +0400

    Add playoffs

--- tournament_system/tournament_system_olimpic.py
+++ tournament_system/tournament_system_olimpic.py
@@ -3,11 +3,22 @@ from math import log
 
 class TournamentSystemOlimpic(tournament_system.TournamentSystem):
     def get_rounds(self):
-        count_of_players = len(self._players_list)
-        count_of_rounds = log(count_of_players)
-        for game in range(count_of_rounds):
-            get_game()
+        list_of_players = self._players_list
+        count_of_players = len(list_of_players)
+        count_of_all_rounds = log(count_of_players)
+        for round in range(count_of_all_rounds):
+            yield get_round(count_of_players)
+            list_of_players = update_list_of_players(count_of_players)
 
-    def get_game(self):
-        for player in range(1, count_of_players, 2):
-            yield [self._players_list[player - 1], self._players_list[player]]
+    def get_round(self, count_of_players):
+        for player in range(1, list_of_players, 2):
+            yield [list_of_players[player - 1], list_of_players[player]]
+
+    def update_list_of_players(self, count_of_players):
+        results = self.get_current_round_results()
+        count_of_players //= 2
+        new_list_of_players = []
+        for game in results.values():
+            new_list_of_players.append(max(game.items(), key=lambda x: x[1]))
+        return new_list_of_players
+
--- tournament_system/tournament_system_olimpic_test.py
+++ tournament_system/tournament_system_olimpic_test.py
@@ -3,18 +3,20 @@ import unittest
 from unittest.mock import Mock
 
 
-PLAYERS_LIST = [1, 2, 3, 4]
-ANSWER = [[1, 2], [3, 4]]
+PLAYERS_LIST = [1, 2, 3, 4, 5, 6, 7, 8]
+RESULTS = {1: {1:2, 2:0}, }
 CONFIG_DATA = ''
 
 
 class TournamentSystemOlimpicTest(unittest.TestCase):
     def test_get_rounds(self):
-        test_round = \
+        test_rounds = \
             tournament_system_olimpic.TournamentSystemOlimpic(PLAYERS_LIST,
                                                               CONFIG_DATA)
-        test_answer = list(test_round.get_rounds())
-        self.assertEqual(test_answer, ANSWER)
+        test_rounds.get_rounds()
+        tmp = test_rounds.get_all_results()
+        #self.assertEqual(test_answer, ANSWER)
+        print(tmp)
 
 
 if __name__ == '__main__':
