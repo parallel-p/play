976fb9c0553a1f49350558d788f71360f144e713
Dmitry Philippov <dmitry239.philippov@mail.ru>
2012-07-13 18:46:15 +0400

    Added config.py

---
+++ games/pepelac/config.py
@@ -0,0 +1,9 @@
+import game_master
+import generator
+import painter
+
+Generator = generator.Generator
+GameMaster = game_master.GameMaster
+Painter = painter.Painter
+
+players_config = 'players_config'
