4abdec2ae2ff6ff825b4fb6b5bb55a8f24a0e01a
Timofey Kukushkin <reebokey@me.com>
2012-07-13 11:18:56 +0400

    Add docs to TournamentSystem

--- tournament_system/tournament_system.py
+++ tournament_system/tournament_system.py
@@ -1,4 +1,8 @@
 class TournamentSystem:
+    '''
+    round_results format:
+    {game_signature: {player: points, ...}, ...}
+    '''
 
     def __init__(self, players_list, config_data):
         self._players_list = players_list
@@ -8,22 +12,40 @@ class TournamentSystem:
         self._current_round_id = 0
 
     def add_round_results(self, round_results):
+        '''
+        Add round_results to all results of tournament
+        '''
         self._results.update(round_results)
         self._current_round_id += 1
 
     def get_all_results(self):
+        '''
+        Return all results of tournament
+        '''
         return self._results
 
     def get_round_results(self, round_id):
+        '''
+        Yield results of round
+        '''
         for game in self.get_all_results():
             if game.round_id == round_id:
                 yield game
 
     def get_current_round_results(self):
+        '''
+        Yield results of current round
+        '''
         return self.get_round_results(self._current_round_id)
 
     def get_rounds(self):
+        '''
+        Yield players_list for every round
+        '''
         pass
 
     def print_table(self):
+        '''
+        Print table in console
+        '''
         pass
