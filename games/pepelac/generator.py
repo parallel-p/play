3927f264996824e38559323105c24430497ab90b
Semyon Bochkaryov <me@gamepr0graamer.ru>
2012-07-13 21:41:39 +0700

    Fixed PEP8

--- games/pepelac/generator.py
+++ games/pepelac/generator.py
@@ -4,19 +4,21 @@ from jury_state import JuryState
 _field_size_x = 50
 _field_size_y = 50
 
+
 class Generator:
     def generate_start_postion(self, game_signature, players_count):
         '''Generates a list of start positions'''
         bullets_count = players_count * 5
         for game in range(players_count):
-            field = [[0 for i in range(_field_size_x)] for j in range(_field_size_y)]
+            field = [[0 for i in range(_field_size_x)]
+                for j in range(_field_size_y)]
             for player in range(players_count):
                 player_x = randint(0, _field_size_x)
                 player_y = randint(0, _field_size_y)
                 while(field[player_x][player_y] != 0):
                     player_x = randint(0, _field_size_x)
                     player_y = randint(0, _field_size_y)
-                field[player_x][player_y] = player+1
+                field[player_x][player_y] = player + 1
             for bullet in range(bullets_count):
                 bullet_x = randint(0, _field_size_x)
                 bullet_y = randint(0, _field_size_y)
