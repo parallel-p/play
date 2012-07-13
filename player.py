21c1afc22d1f1a8499bc191226446d800d97109b
Ivan Uvarov <totosus@gmail.com>
2012-07-13 12:26:16 +0400

    player.py: renamed some variables

--- player.py
+++ player.py
@@ -6,4 +6,4 @@ class Player:
     def __init__(self, cmd_line, author='John Doe', bot='Some_bot'):
         self.author_name = author
         self.bot_name = bot
-        self.path_to_executable = command_line
+        self.command_line = cmd_line
