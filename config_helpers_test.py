e20ad6b6dc10b417fdb5bb1fa2c7f5d992cb12e7
Andreev Alexander <carzil@yandex.ru>
2012-07-13 11:29:01 +0400

    replace " -> ' in config_helpers test

--- config_helpers_test.py
+++ config_helpers_test.py
@@ -6,13 +6,13 @@ import os
 
 class TestConfigHelpers(unittest.TestCase):
     def setUp(self):
-        os.chdir("..")
-        conf_file = open("config.py", "w")
-        conf_file.write("some_value = 0\n")
+        os.chdir('..')
+        conf_file = open('config.py', 'w')
+        conf_file.write('some_value = 0\n')
         conf_file.close()
 
     def test_initialize_game_environment(self):
-        config_helpers.initialize_game_environment("config.py")
+        config_helpers.initialize_game_environment('config.py')
         import config
         self.assertEqual(config.some_value, 0)
 
