71e5e86b5e5544c13d239fcb8e10bf11ae58e2f5
Vadim Drobinin <vadim@drobinin.com>
2012-07-13 13:02:06 +0400

    Add docs

--- tournament_system/tournament_system_olympic.py
+++ tournament_system/tournament_system_olympic.py
@@ -3,7 +3,15 @@ from math import log
 
 
 class TournamentSystemOlympic(tournament_system.TournamentSystem):
+    '''
+    This class spends a game based on the Olympic tournament
+    system, generating a list of players for each round based
+    on the results of the previous one.
+    '''
     def get_rounds(self):
+        '''
+        Yields lists of players for each round.
+        '''
         list_of_players = self._players_list
         count_of_players = len(list_of_players)
         count_of_all_rounds = round(log(count_of_players))
@@ -13,10 +21,16 @@ class TournamentSystemOlympic(tournament_system.TournamentSystem):
             list_of_players = self.update_list_of_players(count_of_players)
 
     def get_round(self, count_of_players, list_of_players):
+        '''
+        Yields list of players for current round.
+        '''
         for player in range(1, count_of_players, 2):
             yield [list_of_players[player - 1], list_of_players[player]]
 
     def update_list_of_players(self, count_of_players):
+        '''
+        Return list of players based on current scores of players.
+        '''
         results = self.get_current_round_results()
         new_list_of_players = []
         for game in results.values():
