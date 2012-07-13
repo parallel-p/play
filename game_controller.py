4593619183a24aeb240733ec29c67fc504c57107
Semyon Bochkaryov <me@gamepr0graamer.ru>
2012-07-13 17:47:07 +0700

    Fixed bug

--- game_controller.py
+++ game_controller.py
@@ -62,7 +62,7 @@ class GameController:
         Creates bots for each player
         '''
         for player in self._players:
-            self.bots[player] = bot.Bot(player.cmd_line, self.config)
+            self.bots[player] = bot.Bot(player.command_line, self.config)
             logger.debug('created bot "%s"', player.bot_name)
             logger.info('all bots created')
 
