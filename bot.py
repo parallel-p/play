3588c2112847a82dbf7e1025f740bbd2c6387097
Vadim Drobinin <vadim@drobinin.com>
2012-07-13 09:37:49 +0400

    Add alpha-version of class Bot and unittests

--- bot.py
+++ bot.py
@@ -1,4 +1,18 @@
-import subprocess
+import psutil
+import threading
+import time
+from subprocess import PIPE
+
+
+'''
+    TODO:
+    while process.is_running():
+        // check limits and `sleep`
+    # to record information about CPU time,
+    # real time and memory
+    # then count of steps became equal to
+    # `y`, check limits and delete information
+'''
 
 
 class ProcessNotRunningException(OSError):
@@ -8,14 +22,26 @@ class ProcessNotRunningException(OSError):
     '''
 
 
-class Player:
+class TimeLimitException(OSError):
     '''
-    This class wraps player's process and stores its information.
+    This exception is raised when bot process exceeded time limit.
+    '''
+
+
+class MemoryLimitException(OSError):
+    '''
+    This exception is raised when bot process exceeded memory limit.
+    '''
+
+
+class Bot:
+    '''
+    This class wraps bot's process and stores its information.
 
     Examples:
         >>> from .move import Move
         >>> from .player_state import PlayerState # import *your* classes
-        >>> p = Player('python bot.py')
+        >>> p = bot('python bot.py')
         >>> p.create_process()
         >>> ps = PlayerState(...)
         >>> move = Move(...)
@@ -23,28 +49,117 @@ class Player:
         <move.Move object at ...>
         >>> p.kill_process()
     '''
-    def __init__(self, player_command):
+    def __init__(self, player_command, config):
         '''
-        Constructor for class Player.
-        player_command is a string which is used to invoke player program.
+        Constructor for class Bot.
+        player_command is a string which is used to invoke bot program.
         '''
         self._player_command = player_command.split()
         self._process = None
+        self._config = config
 
     def create_process(self):
         '''
-        Starts player's process.
+        Starts bot's process.
         '''
-        self._process = subprocess.Popen(
+        self._process = psutil.Popen(
             self._player_command,
-            stdout=subprocess.PIPE,
-            stdin=subprocess.PIPE
+            stdout=PIPE,
+            stdin=PIPE
         )
+
+        self._checker = threading.Thread(target=self._check_memory_limits)
+        self._checker.start()
+
+    def _check_memory_limits(self):
+        '''
+        This function is running in a separate thread
+        and check process exceed memory limit every `CHECK_TIME_SECONDS`
+        seconds.
+        '''
+        CHECK_TIME_SECONDS = 0.15
+        memory_limit_mb = self._config.memory_limit_mb
+
+        exit_code = None
+        while exit_code is None:
+            try:
+                exit_code = self._process.wait(CHECK_TIME_SECONDS)
+            except psutil.TimeoutExpired:
+                pass
+
+            try:
+                megabyte = 1 << 20
+                process_memory = self._process.get_memory_info().rss / megabyte
+            except psutil.NoSuchProcess:
+                return
+            if process_memory > memory_limit_mb:
+                self.kill_process()
+                raise MemoryLimitException
+
+    def _check_cpu_time_limit(self):
+        '''
+        This function is running in a separate thread
+        and check process exceed cpu time limit every `CHECK_TIME_SECONDS`
+        seconds.
+        '''
+        CHECK_TIME_SECONDS = 0.15
+        cpu_limit = self._config.cpu_time_limit_seconds
+        cpu_time_start = self._get_cpu_time()
+
+        exit_code = None
+        while exit_code is None:
+            try:
+                exit_code = self._process.wait(CHECK_TIME_SECONDS)
+            except psutil.TimeoutExpired:
+                pass
+            try:
+                process_cpu_time = self._get_cpu_time()
+            except psutil.NoSuchProcess:
+                return
+            if process_cpu_time is None:
+                return
+            elif process_cpu_time - cpu_time_start > cpu_limit:
+                self.kill_process()
+                raise TimeLimitException
+
+    def _check_real_time_limit(self):
+        '''
+        This function is running in a separate thread
+        and check process exceed real time limit every `CHECK_TIME_SECONDS`
+        seconds.
+        '''
+        CHECK_TIME_SECONDS = 0.15
+        real_limit = self._config.real_time_limit_seconds
+        real_time_start = time.time()
+
+        exit_code = None
+        while (exit_code is None) and (self._is_running()):
+            try:
+                exit_code = self._process.wait(CHECK_TIME_SECONDS)
+            except psutil.TimeoutExpired:
+                exit_code = self._process.wait()
+            try:
+                process_real_time = time.time()
+            except psutil.NoSuchProcess:
+                return
+
+            if process_real_time - real_time_start > real_limit:
+                self.kill_process()
+                raise TimeLimitException
+
+    def _get_cpu_time(self):
+        '''
+        Get CPU time used by bot's process.
+        '''
+        try:
+            return self._process.get_cpu_times().system
+        except psutil.NoSuchProcess:
+            return None
 
     def get_move(self, player_state, move):
         '''
-        Serialize player_state and transfer it to player,
-        then deserialize output received from player to `move`
+        Serialize player_state and transfer it to bot,
+        then deserialize output received from bot to `move`.
         '''
         if not self._is_running:
             raise ProcessNotRunningException
@@ -53,21 +168,32 @@ class Player:
 
     def _read(self, move):
         '''
-        Invokes move.deserialize with player's `stdout`.
+        Invokes move.deserialize with bot's `stdout`.
         `stdout` is a stream opened to read per *byte*.
         '''
+        real_time_limit_seconds = self._config.real_time_limit_seconds
+        cpu_time_limit_seconds = self._config.cpu_time_limit_seconds
+
+        time_cpu_limiter_thread = threading.Thread(
+            target=self._check_cpu_time_limit
+        )
+        time_real_limiter_thread = threading.Thread(
+            target=self._check_real_time_limit
+        )
+        time_cpu_limiter_thread.start()
+        time_real_limiter_thread.start()
         move.deserialize(self._process.stdout)
 
     def _write(self, player_state):
         '''
-        Invokes player_state.serialize with player's `stdin`.
+        Invokes player_state.serialize with bot's `stdin`.
         `stdin` is a stream opened to write per *byte*.
         '''
         player_state.serialize(self._process.stdin)
 
     def kill_process(self):
         '''
-        Kills player's process if it is running or does nothing
+        Kills bot's process if it is running or does nothing
         if the process was already killed.
         '''
         if not self._is_running():
@@ -79,11 +205,11 @@ class Player:
         '''
         Returns true if the process is running and false otherwise.
         '''
-        return self._process and self._process.poll() is None
+        return self._process and self._process.is_running()
 
     def __del__(self):
         '''
-        Destructor for class Player.
-        It automatically kills player's process on delete.
+        Destructor for class bot.
+        It automatically kills bot's process on delete.
         '''
         self.kill_process()
--- bot_test.py
+++ bot_test.py
@@ -1,29 +1,41 @@
-import player
+import bot
 import unittest
+import time
+import psutil
 from unittest.mock import Mock
+from bot import TimeLimitException, MemoryLimitException
 
 
 PLAYER_COMMAND = 'python3 test_game.py'
+ML_PLAYER_COMMAND = 'python3 test_game2.py'
 WRONG_PLAYER_COMMAND = 'fffaaasd test_game.py'
 
+CONFIG = Mock()
+CONFIG.real_time_limit_seconds = 5.0
+CONFIG.cpu_time_limit_seconds = 10.0
+CONFIG.memory_limit_mb = 15.0
+TIME = CONFIG.real_time_limit_seconds
+MEMORY = CONFIG.memory_limit_mb
+NORMAL_TIME = TIME / 5
 
-class PlayerTest(unittest.TestCase):
+
+class BotTest(unittest.TestCase):
     def test_create_process(self):
-        test_player = player.Player(PLAYER_COMMAND)
-        test_player.create_process()
-        self.assertTrue(test_player._is_running())
-        test_player.kill_process()
+        test_bot = bot.Bot(PLAYER_COMMAND, CONFIG)
+        test_bot.create_process()
+        self.assertTrue(test_bot._is_running())
+        test_bot.kill_process()
 
     def test_wrong_command(self):
-        test_player = player.Player(WRONG_PLAYER_COMMAND)
+        test_bot = bot.Bot(WRONG_PLAYER_COMMAND, CONFIG)
         with self.assertRaises(OSError):
-            test_player.create_process()
+            test_bot.create_process()
 
     def test_kill_process(self):
-        test_player = player.Player(PLAYER_COMMAND)
-        test_player.create_process()
-        test_player.kill_process()
-        self.assertFalse(test_player._is_running())
+        test_bot = bot.Bot(PLAYER_COMMAND, CONFIG)
+        test_bot.create_process()
+        test_bot.kill_process()
+        self.assertFalse(test_bot._is_running())
 
     def test_get_move(self):
         def side_effect_for_deserialize(pipe):
@@ -32,8 +44,8 @@ class PlayerTest(unittest.TestCase):
         def side_effect_for_serialize(pipe):
             pipe.write(b'abc\n')
 
-        test_player = player.Player(PLAYER_COMMAND)
-        test_player.create_process()
+        test_bot = bot.Bot(PLAYER_COMMAND, CONFIG)
+        test_bot.create_process()
 
         move = Mock()
         move.deserialize()
@@ -43,10 +55,74 @@ class PlayerTest(unittest.TestCase):
         player_state.serialize()
         player_state.serialize.side_effect = side_effect_for_serialize
 
-        test_player.get_move(player_state, move)
+        test_bot.get_move(player_state, move)
         self.assertTrue(move.deserialize.called)
         self.assertTrue(player_state.serialize.called)
-        test_player.kill_process()
+        test_bot.kill_process()
+
+    def test_time_error(self):
+        def side_effect_for_deserialize(timeout):
+            time.sleep(timeout)
+
+        def side_effect_for_serialize(timeout):
+            time.sleep(timeout)
+
+        def test_without_timelimit_error():
+            move.deserialize.side_effect = \
+                                    side_effect_for_deserialize(NORMAL_TIME)
+            player_state.serialize.side_effect = \
+                                    side_effect_for_serialize(NORMAL_TIME)
+            test_bot.create_process()
+            clock_time = time.clock()
+            test_bot.get_move(player_state, move)
+            test_bot.kill_process()
+            self.assertLessEqual(time.clock(), clock_time + TIME)
+
+        def test_with_timelimit_error():
+            move.deserialize.side_effect = \
+                                    side_effect_for_deserialize(TIME)
+            player_state.serialize.side_effect = \
+                                    side_effect_for_serialize(TIME)
+            test_bot.create_process()
+            test_bot.get_move(player_state, move)
+            self.assertRaises(TimeLimitException)
+            test_bot.kill_process()
+
+        test_bot = bot.Bot(PLAYER_COMMAND, CONFIG)
+        move = Mock()
+        move.deserialize()
+
+        player_state = Mock()
+        player_state.serialize()
+
+        test_without_timelimit_error()
+        test_with_timelimit_error()
+
+    def test_memory_error(self):
+        def test_without_memorylimit_error():
+            test_bot = bot.Bot(PLAYER_COMMAND, CONFIG)
+            test_bot.create_process()
+            test_bot.get_move(player_state, move)
+            # megabyte = 2 ** 20
+            cpu_memory = test_bot._process.get_memory_info().rss / (2 ** 20)
+            test_bot.kill_process()
+            self.assertLessEqual(cpu_memory, MEMORY)
+
+        def test_with_memorylimit_error():
+            test_bot = bot.Bot(ML_PLAYER_COMMAND, CONFIG)
+            test_bot.create_process()
+            test_bot.get_move(player_state, move)
+            self.assertRaises(MemoryLimitException)
+            test_bot.kill_process()
+
+        move = Mock()
+        move.deserialize()
+
+        player_state = Mock()
+        player_state.serialize()
+
+        test_without_memorylimit_error()
+        test_with_memorylimit_error()
 
 if __name__ == '__main__':
     unittest.main()
