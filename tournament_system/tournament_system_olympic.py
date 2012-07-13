8356775486f0c411962d9531a020ea7b2dcb7b40
Vadim Drobinin <vadim@drobinin.com>
2012-07-13 12:54:40 +0400

    Add olympic tournament system and unittests

--- tournament_system/tournament_system_olympic.py
+++ tournament_system/tournament_system_olympic.py
@@ -1,24 +1,25 @@
 import tournament_system
 from math import log
 
-class TournamentSystemOlimpic(tournament_system.TournamentSystem):
+
+class TournamentSystemOlympic(tournament_system.TournamentSystem):
     def get_rounds(self):
         list_of_players = self._players_list
         count_of_players = len(list_of_players)
-        count_of_all_rounds = log(count_of_players)
-        for round in range(count_of_all_rounds):
-            yield get_round(count_of_players)
-            list_of_players = update_list_of_players(count_of_players)
+        count_of_all_rounds = round(log(count_of_players))
+        for game_round in range(count_of_all_rounds + 2):
+            count_of_players = len(list_of_players)
+            yield self.get_round(count_of_players, list_of_players)
+            list_of_players = self.update_list_of_players(count_of_players)
 
-    def get_round(self, count_of_players):
-        for player in range(1, list_of_players, 2):
+    def get_round(self, count_of_players, list_of_players):
+        for player in range(1, count_of_players, 2):
             yield [list_of_players[player - 1], list_of_players[player]]
 
     def update_list_of_players(self, count_of_players):
         results = self.get_current_round_results()
-        count_of_players //= 2
         new_list_of_players = []
         for game in results.values():
-            new_list_of_players.append(max(game.items(), key=lambda x: x[1]))
+            new_list_of_players.append(max(game.items(),
+                                      key=lambda x: x[1])[0])
         return new_list_of_players
-
--- tournament_system/tournament_system_olympic_test.py
+++ tournament_system/tournament_system_olympic_test.py
@@ -1,23 +1,33 @@
-import tournament_system_olimpic
+import tournament_system_olympic
 import unittest
 from unittest.mock import Mock
 
 
 PLAYERS_LIST = [1, 2, 3, 4, 5, 6, 7, 8]
-RESULTS = {1: {1:2, 2:0}, }
+FIRST = {1: {1: 0, 2: 5}, 2: {3: 7, 4: 0}, 3: {5: 8, 6: 4}, 4: {7: 2, 8: 1}}
+SECOND = {5: {2: 9, 3: 1}, 6: {5: 8, 7: 1}}
+THIRD = {7: {2: 4, 5: 7}}
+FIRST_ROUND = [[1, 2], [3, 4], [5, 6], [7, 8]]
+SECOND_ROUND = [[2, 3], [5, 7]]
+THIRD_ROUND = [[2, 5]]
 CONFIG_DATA = ''
 
 
-class TournamentSystemOlimpicTest(unittest.TestCase):
+class TournamentSystemOlympicTest(unittest.TestCase):
     def test_get_rounds(self):
         test_rounds = \
-            tournament_system_olimpic.TournamentSystemOlimpic(PLAYERS_LIST,
+            tournament_system_olympic.TournamentSystemOlympic(PLAYERS_LIST,
                                                               CONFIG_DATA)
-        test_rounds.get_rounds()
-        tmp = test_rounds.get_all_results()
-        #self.assertEqual(test_answer, ANSWER)
-        print(tmp)
-
+        test_rounds.get_current_round_results = Mock()
+        test_rounds.get_current_round_results.side_effect = \
+                [FIRST, SECOND, THIRD]
+        list_of_rounds = list(test_rounds.get_rounds())
+        first_players = list(list_of_rounds[0])
+        second_players = list(list_of_rounds[1])
+        third_players = list(list_of_rounds[2])
+        self.assertEqual(first_players, FIRST_ROUND)
+        self.assertEqual(second_players, SECOND_ROUND)
+        self.assertEqual(third_players, THIRD_ROUND)
 
 if __name__ == '__main__':
     unittest.main()
