d74d6a4f90e11464f17686a82bf86be9fcb0044e
Ivan Uvarov <totosus@gmail.com>
2012-07-13 11:51:45 +0400

    modified:   player.py

--- player.py
+++ player.py
@@ -1,3 +1,9 @@
+from bot import Bot
+
+
 class Player:
-    def __init__(self, cmd_line):
-        self.cmd_line = cmd_line
+
+    def __init__(self, cmd_line, author='John Doe', bot='Some_bot'):
+        self.author_name = author
+        self.bot_name = bot
+        self.path_to_executable = command_line
