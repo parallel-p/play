f9d2f70b1161d795793b9790b5bd677533893352
Ivan Uvarov <totosus@gmail.com>
2012-07-13 12:40:24 +0400

    added test for player

---
+++ player_test.py
@@ -0,0 +1,14 @@
+import unittest
+from player import Player
+
+
+class PlayerTest(unittest.TestCase):
+
+    def test_player:
+        player = Player('python3 bot143.py', 'VasyaPupkin', 'SuperMegaKiller')
+        assert_equal(player.author_name, 'VasyaPupkin')
+        assert_equal(player.bot_name, 'SuperMegaKiller')
+        assert_equal(player.command_line, 'python3 bot143.py')
+
+if __name__=='__main__':
+    unittest.main()
