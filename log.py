f7ffe93db5a11f26613af19a4836066a8f77b756
Arthur Khashaev <arthur@khashaev.ru>
2012-07-13 14:36:31 +0400

    Fixed bugs with formatting in logger

--- log.py
+++ log.py
@@ -1,7 +1,8 @@
 import logging
 
+LOG_FORMAT = '(%(asctime)s)[%(levelname)s]: %(message)s'
+
+logging.basicConfig(format=LOG_FORMAT)
+
 logger = logging.getLogger('play_logger')
 logger.setLevel(logging.DEBUG)
-
-formatter = logging.Formatter('(%(asctime)s)[%(levelname)s]: %(message)s')
-logger.setFormatter(formatter)
