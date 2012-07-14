d8981146b7917871d575dda9bb45844ecf84cb5d
Aganov Artur <artur@Aganov.nout>
2012-07-14 10:58:07 +0400

    added cool test for players_parse function

--- config_helpers_test.py
+++ config_helpers_test.py
@@ -2,6 +2,7 @@ import unittest
 import config_helpers
 import imp
 import os
+import player
 
 
 class TestConfigHelpers(unittest.TestCase):
@@ -17,13 +18,38 @@ class TestConfigHelpers(unittest.TestCase):
         self.assertEqual(config.some_value, 0)
 
 
-class GameTest(unittest.TestCase):
+class TestPlayersParse(unittest.TestCase):
 
     def test_write_logs(self):
-        list = config_helpers.players_parse('test_for_players_parse')
-        for l in list:
-            print('\'' + l.author_name + '\' \'' + l.bot_name + '\'  \'' + l.command_line + '\'')
-
+        test_logs = open('test_for_players_parse', 'w')
+        test_logs.write(""""Author Authorovich" "MegaBot" "/home/artur/bot_path"
+Author Bot "/usr/lol"
+Author "Bot and bot" /usr/lol
+Author "Bot and bot'   '/usr/lol"
+"Author and co"   Bot /usr/lol
+"Author and co" Bot     "/usr/lol"
+"Author and co"      'Bot' /usr/lol
+Author   Bot     /usr/lol
+  '   '   a   '   '  """)
+        test_logs.close()
+        gotten_list = config_helpers.players_parse('test_for_players_parse')
+        answer_list = list()
+        answer_list.append(player.Player('/home/artur/bot_path', 'Author Authorovich', 'MegaBot'))
+        answer_list.append(player.Player('/usr/lol', 'Author', 'Bot'))
+        answer_list.append(player.Player('/usr/lol', 'Author', 'Bot and bot'))
+        answer_list.append(player.Player('/usr/lol', 'Author', 'Bot and bot'))
+        answer_list.append(player.Player('/usr/lol', 'Author and co', 'Bot'))
+        answer_list.append(player.Player('/usr/lol', 'Author and co', 'Bot'))
+        answer_list.append(player.Player('/usr/lol', 'Author and co', 'Bot'))
+        answer_list.append(player.Player('/usr/lol', 'Author', 'Bot'))
+        answer_list.append(player.Player('   ', '   ', 'a'))
+        #fuck yeah
+        for i in range(9):
+            self.assertEqual(answer_list[i].bot_name, gotten_list[i].bot_name)
+            self.assertEqual(answer_list[i].author_name, gotten_list[i].author_name)
+            self.assertEqual(answer_list[i].command_line, gotten_list[i].command_line)
+        os.remove('test_for_players_parse')
+        
 
 if __name__ == '__main__':
     unittest.main()
