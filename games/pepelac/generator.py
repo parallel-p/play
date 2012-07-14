ecede1e76e6e2e246572686667c56cca6dce81c7
Semyon Bochkaryov <me@gamepr0graamer.ru>
2012-07-14 15:05:04 +0700

    Fixed bug

--- games/pepelac/generator.py
+++ games/pepelac/generator.py
@@ -2,30 +2,29 @@ import random
 import copy
 from jury_state import JuryState
 
-_field_size_x = 50
-_field_size_y = 50
+_field_size = 50
 
 
 class Generator:
-    def generate_players(field):
+    def generate_players(self, field, players_count):
         new_field = copy.deepcopy(field)
-        for player in range(self.players_count):
-            player_x = randint(0, _field_size_x)
-            player_y = randint(0, _field_size_y)
+        for player in range(players_count):
+            player_x = random.randint(0, _field_size)
+            player_y = random.randint(0, _field_size)
             while(new_field[player_x][player_y] != 0):
-                player_x = randint(0, _field_size_x)
-                player_y = randint(0, _field_size_y)
+                player_x = random.randint(0, _field_size)
+                player_y = random.randint(0, _field_size)
             new_field[player_x][player_y] = player + 1
         return new_field
 
-    def generate_bullets(field):
+    def generate_bullets(self, field, bullets_count):
         new_field = copy.deepcopy(field)
         for bullet in range(bullets_count):
-            bullet_x = randint(0, _field_size_x)
-            bullet_y = randint(0, _field_size_y)
+            bullet_x = random.randint(0, _field_size)
+            bullet_y = random.randint(0, _field_size)
             while(new_field[bullet_x][bullet_y] != 0):
-                bullet_x = randint(0, _field_size_x)
-                bullet_y = randint(0, _field_size_y)
+                bullet_x = random.randint(0, _field_size)
+                bullet_y = random.randint(0, _field_size)
             new_field[bullet_x][bullet_y] = -1
         return new_field
 
@@ -34,9 +33,10 @@ class Generator:
         '''Generates a list of start positions'''
         random.seed()
         self.bullets_count = self.players_count * 5
+        self.time = self.players_count * randint(40, 60)
         for game in range(players_count):
-            field = [[0 for i in range(_field_size_x)]
-                for j in range(_field_size_y)]
-            field = generate_players(field)
-            field = generate_bullets(field)
-            yield JuryState(field)
+            field = [[0 for i in range(_field_size)]
+                for j in range(_field_size)]
+            field = generate_players(field, self.players_count)
+            field = generate_bullets(field, self.bullets_count)
+            yield JuryState(_field_size, field, self.time)
