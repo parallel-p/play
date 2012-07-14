253f34ea6f2f2f062bfa8b4c62ef79d2b1ba7301
Arthur Khashaev <arthur@khashaev.ru>
2012-07-14 09:55:03 +0400

    Fixed config, serialization, deserialization and tests for it

--- games/nim/config.py
+++ games/nim/config.py
@@ -7,3 +7,7 @@ GameMaster = game_master.GameMaster
 Painter = painter.Painter
 
 players_config = 'players_config'
+
+real_time_limit_seconds = 5.0
+cpu_time_limit_seconds = 10.0
+memory_limit_mb = 15.0
--- games/nim/move_tests.py
+++ games/nim/move_tests.py
@@ -5,7 +5,7 @@ from move import deserialize
 
 class MoveTests(unittest.TestCase):
     def test_deserialization(self):
-        stream = io.StringIO('1 1\n1 100')
+        stream = io.BytesIO(b'1 1\n1 100')
         move = deserialize(stream)
         self.assertEqual(move[0], 1)
         self.assertEqual(move[1], 1)
