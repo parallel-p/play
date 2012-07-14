3bb5ab71c6f45a799b150f4481cd07288c33eb12
Andreev Alexander <carzil@yandex.ru>
2012-07-14 09:30:09 +0400

    datetime format in logs

--- log.py
+++ log.py
@@ -2,7 +2,7 @@ import logging
 
 LOG_FORMAT = '(%(asctime)s)[%(levelname)s]: %(message)s'
 
-logging.basicConfig(format=LOG_FORMAT)
+logging.basicConfig(format=LOG_FORMAT, , datefmt='%m/%d/%Y %H:%M:%S %p')
 
 logger = logging.getLogger('play_logger')
 logger.setLevel(logging.DEBUG)
