cf3220622f503bbbe844084f4e6dfb510c455b12
Andreev Alexander <carzil@yandex.ru>
2012-07-14 09:55:01 +0400

    fix syntax error in log.py

--- log.py
+++ log.py
@@ -2,7 +2,7 @@ import logging
 
 LOG_FORMAT = '(%(asctime)s)[%(levelname)s]: %(message)s'
 
-logging.basicConfig(format=LOG_FORMAT, , datefmt='%m/%d/%Y %H:%M:%S %p')
+logging.basicConfig(format=LOG_FORMAT, datefmt='%m.%d.%Y %H:%M:%S')
 
 logger = logging.getLogger('play_logger')
 logger.setLevel(logging.DEBUG)
