f85c9939fbeaddd337e17d50f2d76577a78b1e25
Dmitry Tomp <dima_tomp@inbox.ru>
2012-07-14 10:06:56 +0300

    Moved everything to the root

--- video_visualizer_test.py
+++ video_visualizer_test.py
@@ -5,33 +5,33 @@ import visualizer
 import os
 import subprocess
 import pickle
-import shutil
-from game_signature import GameSignature
+import shutil                                  
+from tournament_stages.game_signature import GameSignature   
 
 
 # Unfortunately, Mock objects cannot be pickled, so we have to do like this:
 class GameController:
     def __init__(self):
-        self.jury_states = []
-        self.signature = GameSignature()
+        self.jury_states = []                                   
+        self.signature = GameSignature()                        
 
     def __lt__(self, other):
         return self.signature < other.signature
 
-
+                                                                                               
 class VideoVisualizerTest(unittest.TestCase):
     def setUp(self):
         if not os.path.exists('test'):
-            os.mkdir('test')
+            os.mkdir('test')   
         os.chdir('test')
 
         # There should be an image folder containing GIF images with specified
-        # names (see below).   
+        # names (see below).
         painter_obj = Mock()
         painter_obj.paint = lambda x: open(os.path.join('images', x),
                                            'rb').read()
 
-        self.viz = visualizer.VideoVisualizer(2, painter_obj, '.*\.gc', 'test')
+        self.viz = visualizer.VideoVisualizer(3, painter_obj, '.*\.gc', 'test')
 
     def test_collect_game_images_to_video(self):
         os.chdir('..')
@@ -45,8 +45,8 @@ class VideoVisualizerTest(unittest.TestCase):
         self.assertTrue(os.path.exists(visualizer.TEMPFILE_NAME))
         for fname in os.listdir('test'):
             self.assertTrue(fname.endswith('.gc') or fname == 'result.avi')
-
-    def test_compile(self):
+                               
+    def test_compile(self):    
         # Generate many random GameControllers.
         gc = GameController()
         for gc.signature.tournament_id in range(randint(1, 3)):
@@ -63,20 +63,23 @@ class VideoVisualizerTest(unittest.TestCase):
         os.chdir('..')
         if os.path.exists(visualizer.TEMPFILE_NAME):
             os.remove(visualizer.TEMPFILE_NAME)
+        if os.path.exists('result.avi'):
+            os.remove('result.avi')
 
         self.viz.compile('result.avi')
 
         self.assertFalse(os.path.exists(visualizer.TEMPFILE_NAME))
         self.assertFalse(os.path.exists('result.avi'))
-        # Check whether all images have been removed:
+        # Check whether all images have been removed:           
         for fname in os.listdir('test'):
             self.assertTrue(fname.endswith('.gc') or fname == 'result.avi')
 
-    def tearDown(self):
+    def tearDown(self):        
         # To be able to watch the resulting video file manually, let's copy it.
         if os.path.exists(os.path.join('test', 'result.avi')):
             shutil.copyfile(os.path.join('test', 'result.avi'), 'result.avi')
         shutil.rmtree('test')
+
 
 if __name__ == '__main__':
     unittest.main()
