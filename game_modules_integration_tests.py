aa57b78e561b50f897ddf1511014690e33acaecc
Arthur Khashaev <arthur@khashaev.ru>
2012-07-14 12:35:33 +0400

    Added test for game modules intergration tests

--- game_modules_integration_tests.py
+++ game_modules_integration_tests.py
@@ -28,5 +28,11 @@ class GameModulesIntegrationTests(unittest.TestCase):
         controller = self._simulator.play()
         self.assertTrue(controller.is_finished)
 
+    def test_that_scores_exists_for_each_player(self):
+        controller = self._simulator.play()
+        scores = controller.get_scores()
+        players = controller.get_players()
+        self.assertEqual(set(scores.keys()), set(players))
+
 if __name__ == '__main__':
     unittest.main()
