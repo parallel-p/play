cd7e36565e61afdeccfc3874da1d842cd58ffc67
Andrey Zaytsev <andzaytsev@gmail.com>
2012-07-14 10:50:30 +0400

    Fixed test, now it works

--- game_simulator_test.py
+++ game_simulator_test.py
@@ -6,18 +6,23 @@ from unittest.mock import Mock
 
 class GameSimulatorTest(unittest.TestCase):
     def test_game(self):
-        def game_side_effect(game_controller):
-            game_controller.is_finished = 1
+        def game_master_side_effect(game_controller, jury_state):
+            game_master._game_controller = game_controller
+            return game_master
+
+        def game_side_effect(jury_state):
+            game_master._game_controller.is_finished = True
         game_master = Mock()
+        game_master.is_finished = False
         game_master.tick.side_effect = game_side_effect
         config = Mock()
-        config.game_master_constructor.return_value = game_master
+        config.GameMaster.side_effect=game_master_side_effect
         eng = game_simulator.GameSimulator(config, [], [], [])
         result = eng.play()
         game_master.tick.assert_called_once()
 
         answer = game_controller.GameController(config, [], [], [])
-        answer.is_finished = 1
+        answer.is_finished = True
         self.assertEqual(answer.is_finished, result.is_finished)
 
 
