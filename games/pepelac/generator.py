47438714983c0697901a5f32bea52936089b0875
Semyon Bochkaryov <me@gamepr0graamer.ru>
2012-07-14 16:08:33 +0700

    Fixed some bugs

--- games/pepelac/generator.py
+++ games/pepelac/generator.py
@@ -9,22 +9,22 @@ class Generator:
     def generate_players(self, field, players_count):
         new_field = copy.deepcopy(field)
         for player in range(players_count):
-            player_x = random.randint(0, _field_size)
-            player_y = random.randint(0, _field_size)
+            player_x = random.randint(0, _field_size-1)
+            player_y = random.randint(0, _field_size-1)
             while(new_field[player_x][player_y] != 0):
-                player_x = random.randint(0, _field_size)
-                player_y = random.randint(0, _field_size)
+                player_x = random.randint(0, _field_size-1)
+                player_y = random.randint(0, _field_size-1)
             new_field[player_x][player_y] = player + 1
         return new_field
 
     def generate_bullets(self, field, bullets_count):
         new_field = copy.deepcopy(field)
         for bullet in range(bullets_count):
-            bullet_x = random.randint(0, _field_size)
-            bullet_y = random.randint(0, _field_size)
+            bullet_x = random.randint(0, _field_size-1)
+            bullet_y = random.randint(0, _field_size-1)
             while(new_field[bullet_x][bullet_y] != 0):
-                bullet_x = random.randint(0, _field_size)
-                bullet_y = random.randint(0, _field_size)
+                bullet_x = random.randint(0, _field_size-1)
+                bullet_y = random.randint(0, _field_size-1)
             new_field[bullet_x][bullet_y] = -1
         return new_field
 
@@ -33,7 +33,7 @@ class Generator:
         '''Generates a list of start positions'''
         random.seed()
         self.bullets_count = self.players_count * 5
-        self.time = self.players_count * randint(40, 60)
+        self.time = self.players_count * random.randint(40, 60)
         for game in range(players_count):
             field = [[0 for i in range(_field_size)]
                 for j in range(_field_size)]
