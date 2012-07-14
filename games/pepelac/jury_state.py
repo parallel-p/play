bf5d0b0924e6e40750e121e55a234bf5c740fa0f
Dmitry Philippov <dmitry239.philippov@mail.ru>
2012-07-14 10:54:06 +0400

    Now JuryState takes the number of bullets of each player too

--- games/pepelac/jury_state.py
+++ games/pepelac/jury_state.py
@@ -1,5 +1,5 @@
 class JuryState:
-    def __init__(self, field_side, field, explosion_time):
+    def __init__(self, field_side, field, bullets, explosion_time):
         '''
         field_side is side of field
         field is the current field:
@@ -8,8 +8,11 @@ class JuryState:
         if field[i][j] = 0 then cell (i, j) is empty
         if field[i][j] > 1 then there is player in cell (i, j)
 
+        bullets is list of quantities of bullets, which players have
+
         explosion_time is time before the explosion of the field
         '''
         self.field_side = field_side
         self.field = field
+        self.bullets = bullets
         self.explosion_time = explosion_time
