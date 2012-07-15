2ae83cb2eda982c082a7ad17322ea107356fd26c
Arthur Khashaev <arthur@khashaev.ru>
2012-07-15 15:49:27 +0400

    Fixed pepelac's config to satisfy requirements

--- games/pepelac/config.py
+++ games/pepelac/config.py
@@ -7,3 +7,9 @@ GameMaster = game_master.GameMaster
 Painter = painter.Painter
 
 players_config = 'players_config'
+
+real_time_limit_seconds = 5.0
+cpu_time_limit_seconds = 10.0
+memory_limit_mb = 15.0
+
+tournament_system = 'olympic'
