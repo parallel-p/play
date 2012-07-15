3596bf526efa09da1422678df98d0a0babfa4033
Andreev Alexander <carzil@yandex.ru>
2012-07-15 13:49:33 +0400

    added run_tests.py util

---
+++ run_tests.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import unittest
+from log import logger
+
+def main():
+    # no log messages
+    logger.setLevel(1000000000000)
+
+    loader = unittest.TestLoader()
+    suite = loader.discover(".", "*_tests.py")
+    test_runner = unittest.TextTestRunner(verbosity=2)
+    test_runner.run(suite)
+
+if __name__ == '__main__':
+    main()
