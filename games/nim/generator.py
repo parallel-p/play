6f613479deba79fccaf2ed9729ff253373d855a2
Andreev Alexander <carzil@yandex.ru>
2012-07-15 16:54:24 +0400

    heaps count added

--- games/nim/generator.py
+++ games/nim/generator.py
@@ -3,13 +3,14 @@ from jury_state import JuryState
 
 
 class Generator:
-    _max_stones_count = 10
-    _games_count = 1
+    _max_stones_count = 100
+    _games_count = 2
+    _heaps_count = 15
 
     def generate_start_positions(self, game_signature, players_count):
         '''Generates a list of start positions (Jury states)'''
         for game in range(self._games_count):
             heap_sizes = []
-            for heap in range(players_count):
+            for heap in range(self._heaps_count):
                 heap_sizes.append(random.randint(1, self._max_stones_count))
             yield JuryState(heap_sizes)
