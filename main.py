5fe320a0de69a0948989d456283771f7acb73cd7
Pavel Dubov <dubov94@gmail.com>
2012-07-15 09:36:48 +0400

    ENLGISH, DO YOU SPEAK IT

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ with function initialize_game_environment it add game_path to sys.path
 import config_helpers
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        raise MainArgumentException('Many or little arguments for main.py')
+        raise MainArgumentException('Arguments count mismatch')
     game_path = sys.argv[1]
     config_helpers.initialize_game_environment(game_path)
 import config
