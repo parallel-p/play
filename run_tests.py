c423b6d392ec7d73c3649fc0324be89ec19623c7
Andreev Alexander <carzil@yandex.ru>
2012-07-15 16:21:59 +0400

    fix test's pattern in run_tests

--- run_tests.py
+++ run_tests.py
@@ -7,7 +7,7 @@ def main():
     logger.setLevel(1000000000000)
 
     loader = unittest.TestLoader()
-    suite = loader.discover(".", "*_tests.py")
+    suite = loader.discover(".", "*_test.py")
     test_runner = unittest.TextTestRunner(verbosity=2)
     test_runner.run(suite)
 
