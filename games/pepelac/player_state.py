da08ee4c504ad5c91f52939ecad4a26d75c9f762
Arthur Khashaev <arthur@khashaev.ru>
2012-07-15 17:54:41 +0400

    Removed redundant self from serializator functions in pepelac-game

--- games/pepelac/player_state.py
+++ games/pepelac/player_state.py
@@ -1,19 +1,20 @@
-def serialize_field_side(self, field_side, stream):
+def serialize_field_side(field_side, stream):
     stream.write(str(field_size) + '\n')
     stream.flush()
 
-    
+
 class PlayerState:
-    explosion_time=None
-    current_player=None
-    players=[]
-    bullets=[]
+    explosion_time = None
+    current_player = None
+    players = []
+    bullets = []
 
 
 def list_to_str(lst):
     return ' '.join(lst)
 
-def serialize_pstate(self, ps, stream):
+
+def serialize_pstate(ps, stream):
     stream.write('\n'.join([
         list_to_str([len(ps.players) + 1, len(ps.bullets), ps.explosion_time]),
         list_to_str(ps.current_player),
