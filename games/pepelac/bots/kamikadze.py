3fbbfa1355f5cc776ec1f1b45809414808f050d7
Timofey Kukushkin <reebokey@me.com>
2012-07-16 10:13:38 +0400

    Improve kamikadze bot

--- games/pepelac/bots/kamikadze.py
+++ games/pepelac/bots/kamikadze.py
@@ -1,19 +1,23 @@
 import sys
 
 
-input()
+n = input()
 
 while True:
-    input()
+    p, b, k = tuple(map(int, input().split()))
     x, y = tuple(map(int, input().split()))[:2]
 
-    while not x == y == 1:
-        if x > 1:
-            x -= 1
-            print('LEFT')
-        elif y > 1:
-            y -= 1
-            print('UP')
-        sys.stdout.flush()
-    print('STAND')
+    if p == 2:
+        x2, y2 = tuple(map(int, input().split()))[:2]
+    else:
+        x2, y2 = -1, -1
+
+    [input() for i in range(b)]
+
+    if x > 1 and not (x2 == x - 1 and y2 == y):
+        print('LEFT')
+    elif y > 1 and not (y2 == y - 1 and x2 == x):
+        print('UP')
+    else:
+        print('STAND')
     sys.stdout.flush()
