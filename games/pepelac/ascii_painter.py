11d9f8f7b14efc5b4ac1808cc52afca975b82edb
Ivan Uvarov <totosus@gmail.com>
2012-07-16 14:31:05 +0400

    added ASCII painter

---
+++ games/pepelac/ascii_painter.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+
+def set_color(color, print_result=False):
+    '''takes a 3-tuple with codes for Background, Foreground and Style and
+    returns a string with ANSI escape codes for each of them.
+    Any of them can be None, in which case the respective codes will not be
+    added to the string.
+    If print_result is True, then it also writes these codes to stdout'''
+    (back, fore, style) = color
+    from sys import stdout
+    CSI = '\x1b['
+    backc = ['40', '44', '46', '42', '45', '41', '49', '47', '43']
+    forec = ['30', '34', '36', '32', '35', '31', '39', '37', '33']
+    stylec = ['1', '2', '22', '0']
+    code = ''
+    for param, arg in zip([backc, forec, stylec], [back, fore, style]):
+        if arg is not None:
+            code += (CSI + param[arg] + 'm')
+    if print_result:
+        stdout.write(code)
+    return(code)
+
+
+class Painter():
+
+    class Cell():
+
+        def __init__(self, val, color):
+            self.value = val
+            self.color = color
+
+    def __init__(self, chars=('@@', '[]', '**', 'P{:1x}'),
+    colors=((5, 8, 0), (3, 2, 2), (8, 0, 2), (4, 7, 0))):
+        self.chars = chars
+        self.colors = colors
+
+    def _cell_line(self, line):
+        out = []
+        for pos in line:
+            if pos == -2:
+                cell = self.Cell(self.chars[0], self.colors[0])
+                # default: bright yellow '@@' on red
+            elif pos == 0:
+                cell = self.Cell(self.chars[1], self.colors[1])
+                # default: cyan '[]' on green
+            elif pos == -1:
+                cell = self.Cell(self.chars[2], self.colors[2])
+                # default: black '**' on yellow
+            else:
+                cell = self.Cell(self.chars[3].format(pos), self.colors[3])
+                # default: bright white 'P{hex number of player}' on magenta
+            out.append(cell)
+        return out
+
+    def _generate_line(self, cell_line):
+        prev_color = (None, None, 3) # total reset
+        text_line = ''
+        for cell in cell_line:
+            # if the color of this cell is the same as
+            #the previous one's, we need not change it
+            if prev_color != cell.color:
+                text_line += set_color(cell.color, False)
+            text_line += cell.value
+            prev_color = cell.color
+        # reset the color in case this is the last line
+        text_line += set_color((None, None, 3), False)
+        return text_line
+
+    def ascii_paint(self, jury_state):
+        text_field = ''
+        cell_field = [list(self._cell_line(fc)) for fc in jury_state.field]
+        for line in cell_field:
+            text_field += self._generate_line(line) + '\n'
+        return text_field
---
+++ games/pepelac/test_ascii_painter.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from ascii_painter import Painter
+import ascii_painter_test_data
+import unittest
+from unittest.mock import Mock
+
+class ascii_painter_test(unittest.TestCase):
+
+    def test_output(self):
+        test_subject = Painter()
+        mock_js = Mock()
+        for test in ascii_painter_test_data.tests:
+            mock_js.field = test[0]
+            picture = test_subject.ascii_paint(mock_js)
+            self.assertEqual(test[1], picture)
+
+if __name__ == '__main__':
+    unittest.main()
