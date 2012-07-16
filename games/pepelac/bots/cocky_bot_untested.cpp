831f0a8db72aa5466b63a251833fc440872820af
Aganov Artur <artur@Aganov.nout>
2012-07-16 09:55:52 +0400

    added cocky bot. it needs to test

---
+++ games/pepelac/bots/cocky_bot_untested.cpp
@@ -0,0 +1,231 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <stdlib.h>
+#include <set>
+#include <map>
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+
+struct Player{
+   int x, y, b;
+};
+
+Player player(int x, int y, int b){
+  Player t;
+  t.x = x;
+  t.y = y;
+  t.b = b;
+  return t;
+}
+
+struct Bullet{
+   int x, y, c;
+};
+
+Bullet bullet(int x, int y){
+  Bullet t;
+  t.x = x;
+  t.y = y;
+  t.c = 0;
+  return t;
+}
+
+
+int n, p, k, b, dan[100][100];
+bool isbul[100][100];
+int big = 1000000000;
+vector<Player> players;
+vector<Bullet> bullets;
+
+
+bool up(){
+  if (players[0].y == 1){
+    return 0;
+  }
+  else{
+    players[0].y--;
+    cout << "Up" << endl;
+    cout.flush();
+    return 1;
+  }
+}
+
+bool down(){
+  if (players[0].y == n){
+    return 0;
+  }
+  else{
+    players[0].y++;
+    cout << "Down" << endl;
+    cout.flush();
+    return 1;
+  }
+}
+
+bool left(){
+  if (players[0].x == 1){
+    return 0;
+  }
+  else{
+    players[0].x--;
+    cout << "Left" << endl;
+    cout.flush();
+    return 1;
+  }
+}
+
+bool right(){
+  if (players[0].x == n){
+    return 0;
+  }
+  else{
+    players[0].x++;
+    cout << "Right" << endl;
+    cout.flush();
+    return 1;
+  }
+}
+
+bool stay(){
+  cout << "Stay" << endl;
+  cout.flush();
+  return 1;
+}
+
+
+
+void read(){
+  int i, t1, t2, t3 ,j;
+  cin >> p >> b >> k;//bullets, players, rounds before armagedonn
+
+  players.clear();
+  for (i = 0; i < p; i++){
+    cin >> t1 >> t2 >> t3;
+    players.push_back(player(t1, t2, t3));
+  }
+
+  bullets.clear();
+  for (i = 0; i < b; i++){
+    cin >> t1 >> t2;
+    bullets.push_back(bullet(t1, t2));
+  }
+}
+
+void armageddon(){
+  int i, j, t, s;
+  for (i = 1; i <= n; i++){
+    for (j = 1; j <= n; j++){
+      dan[i][j] = 0;
+      isbul[i][j] = 0;
+    }
+  }
+
+  t = k;
+
+  for (s = 1; t <= 0; s++){
+    for (i = s; i <= n - s && t <= 0; i++){
+      dan[i][s] = big;
+      t++;
+    }
+    for (i = s; i <= n - s && t <= 0; i++){
+      dan[s][i] = big;
+      t++;
+    }
+    for (i = n - s + 1; i >= s + 1 && t <= 0; i--){
+      dan[i][n - s + 1] = big;
+      t++;
+    }
+    for (i = n - s + 1; i >= s + 1 && t <= 0; i--){
+      dan[n - s + 1][i] = big;
+      t++;
+    }
+  }
+}
+
+void go(int x, int y){
+  bool f;
+
+  if (players[0].x < x && !f && dan[players[0].x + 1][players[0].y] < big)
+    right();
+  if (players[0].x > x && !f && dan[players[0].x - 1][players[0].y] < big)
+    left();
+  if (players[0].y < y && !f && dan[players[0].x][players[0].y + 1] < big)
+    down();
+  if (players[0].y > y && !f && dan[players[0].x][players[0].y - 1] < big)
+    up();
+  if (!f)
+    stay();
+}
+
+int main(){
+  //freopen("input.txt", "r", stdin);
+  //freopen("outputtxt", "w", stdout);
+  big = n;
+  int i, f, i1, j1;
+  Bullet bb;
+  cin >> n;//field size
+
+  while (1){
+    read();
+    //cout << "read" << endl;
+
+    armageddon();
+
+    //cout << "armageddoned" << endl;
+
+    if (b > 0 && players[0].b <= players[1].b){
+
+      for (i = 0; i < b; i++)
+        isbul[bullets[i].x][bullets[i].y] = 1;
+
+      for (i = 0; i < b; i++){
+        bullets[i].c = 2 * n - abs(bullets[i].x - players[0].x) - abs(bullets[i].y - players[0].y);
+        if (players[0].b < players[1].b){
+          bullets[i].c += abs(bullets[i].x - players[1].x) + abs(bullets[i].y - players[1].y);
+        }
+        else{
+          bullets[i].c += 2 * n;
+        }
+
+        for (i1 = max(1, bullets[i].x - 3); i1 <= min(n, bullets[i].x + 3); i1++){
+          for (j1 = max(1, bullets[i].y - 3); j1 <= min(n, bullets[i].y + 3); j1++){
+            bullets[i].c += isbul[i1][j1] * n / 4;
+          }
+        }
+      }
+
+      bb = bullets[0];
+      for (i = 1; i < b; i++){
+        if (bb.c < bullets[i].c)
+          bb = bullets[i];
+      }
+      //cout << "going to" << " " << bb.x << " " << bb.y << endl;
+      go(bb.x, bb.y);
+    }
+    else if (players[0].b >= players[1].b + b){
+      go(n / 2 + 1, n / 2 + 1);
+    }
+    else if (players[0].b > players[1].b){
+      go(players[1].x, players[1].y);
+    }
+    else{
+      stay();
+      //failed
+    }
+
+    //cout << "steped" << endl;
+
+  }
+
+
+
+
+
+
+  return 0;
+}
