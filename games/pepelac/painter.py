0e2be601618feb68e3cd7b088a904f6f6d704f1f
Timofey Kukushkin <reebokey@me.com>
2012-07-16 17:54:23 +0400

    Add new images and add gunplay visualization

--- games/pepelac/painter.py
+++ games/pepelac/painter.py
@@ -1,10 +1,11 @@
 ﻿# -*- coding: utf-8 -*-
 
 FRAME_SIDE = 1024
-FREE_SIDE = 1024
-RIGHT_MARGIN = 256
+FREE_SIDE = 512
+RIGHT_MARGIN = 50
 SMALL_SIDE = 50
 MARGIN = 20
+LINE_WIDTH = 1
 
 
 def set_color(color, print_result=False):
@@ -47,8 +48,9 @@ class Painter:
             self.value = val
             self.color = color
 
-    def __init__(self, players):
+    def __init__(self, players, fight=False):
         self.players = players
+        self.fight = fight
 
     def _initialize(self, jury_state):
         from PIL import Image, ImageDraw, ImageFont
@@ -59,12 +61,12 @@ class Painter:
         self._cell_side = FRAME_SIDE / jury_state.field_side
 
     def draw_on_the_left(self, players, text, color, image):
-        font = ImageFont.truetype('times.ttf', 30)
+        font = ImageFont.truetype('times.ttf', 40)
         draw = ImageDraw.Draw(image)
 
-        x = 100
-        y = players * (SMALL_SIDE + MARGIN)
-        draw.text((0, y + SMALL_SIDE / 4),
+        x = 145
+        y = players * (SMALL_SIDE + MARGIN) + 45
+        draw.text((45, y + SMALL_SIDE / 5),
                   text,
                   fill='black',
                   font=font
@@ -119,8 +121,10 @@ class Painter:
         Paints the state of the game;
         Returns the string --- bits of the jpeg image.
         '''
+        width = FRAME_SIDE + FREE_SIDE + RIGHT_MARGIN
+        height = FRAME_SIDE
         image = Image.new('RGBA',
-                          (FRAME_SIDE + FREE_SIDE + RIGHT_MARGIN, FRAME_SIDE),
+                          (width, height),
                           'white'
                           )
 
@@ -130,71 +134,109 @@ class Painter:
         '''
         Start drawing field
         '''
-        colors = ['', 'black', 'yellow', 'green', 'pink', 'red', 'blue']
+        colors = ['', 'red', 'blue', 'green', 'pink', 'black', 'yellow']
         players_count = 0
 
-        fire_ico = image_resize('images/fire.jpg', self._cell_side - 10)
-        patron_ico = image_resize('images/patron.jpg', self._cell_side - 10)
-        player_ico = image_resize('images/player.png', self._cell_side - 10)
+        draw = ImageDraw.Draw(image)
 
-        for i, row in enumerate(jury_state.field):
-            for j, cell in enumerate(row):
-                x = i * self._cell_side
-                y = j * self._cell_side + FREE_SIDE
-                rectangle = (y, x, y + self._cell_side, x + self._cell_side)
+        for num, player in enumerate(self.players):
+            name = self.players[players_count].author_name
+            self.draw_on_the_left(num + 1,
+                                  player.author_name,
+                                  colors[num + 1], image
+                                  )
 
-                draw = ImageDraw.Draw(image)
+        if not self.fight:
+            fire_ico = image_resize('images/fire.png', self._cell_side - 4)
+            patron_ico = image_resize('images/patron.png', self._cell_side - 4)
+            player_ico = image_resize('images/player.png', self._cell_side - 4)
 
-                if cell == -2:
-                    draw.rectangle(rectangle, outline='black')
-                    image.paste(fire_ico, (y + 5, x + 5))
-                elif cell == -1:
-                    draw.rectangle(rectangle, fill='purple', outline='black')
-                    image.paste(patron_ico, (y + 5, x + 5))
-                elif cell == 0:
-                    draw.rectangle(rectangle, outline='black')
-                else:
-                    name = self.players[players_count].author_name
-                    self.draw_on_the_left(players_count,
-                                          name,
-                                          colors[cell], image
-                                          )
-                    draw.rectangle(rectangle,
-                                   fill=colors[cell],
-                                   outline='black'
-                                   )
-                    players_count += 1
-                    image.paste(player_ico, (y + 5, x + 5), player_ico)
+            for x in range(FREE_SIDE, FREE_SIDE + FRAME_SIDE + 1,
+                           self._cell_side):
+                draw.line([(x, 0), (x, FRAME_SIDE)], width=LINE_WIDTH,
+                          fill='grey')
 
-                del draw
+            for y in range(0, FRAME_SIDE + 1, self._cell_side):
+                draw.line([(FREE_SIDE, y), (FREE_SIDE + FRAME_SIDE, y)],
+                          width=LINE_WIDTH, fill='grey')
+
+            for i, row in enumerate(jury_state.field):
+                for j, cell in enumerate(row):
+                    x = i * self._cell_side
+                    y = j * self._cell_side + FREE_SIDE
+                    rectangle = (y + 1, x + 1,
+                                 y + self._cell_side - 1,
+                                 x + self._cell_side - 1)
+                    rect = (y + 1, x + 1,
+                            y + self._cell_side - 1, x + self._cell_side - 1)
+                    if cell == -2:
+                        draw.rectangle(rect, fill='black')
+                        image.paste(fire_ico, (y + 2, x + 2), fire_ico)
+                    elif cell == -1:
+                        #draw.rectangle(rect, fill='orange')
+                        image.paste(patron_ico, (y + 2, x + 2), patron_ico)
+                    elif cell == 0:
+                        pass
+                    else:
+                        draw.rectangle(rectangle, fill=colors[cell])
+                        image.paste(player_ico, (y + 2, x + 2), player_ico)
+        else:
+            player_ico = image_resize('images/player.png', width / 3)
+            size = player_ico.size[0]
+            rectangle = (width / 8, height / 5,
+                         width / 8 + size - 5, height / 5 + size - 5)
+            draw.rectangle(rectangle, fill='red')
+            rectangle = (width / 8 + width / 2, height / 5,
+                         width / 8 + width / 2 + size - 5,
+                         height / 5 + size - 5)
+            draw.rectangle(rectangle, fill='blue')
+            image.paste(player_ico, (width / 8, height / 5), player_ico)
+            image.paste(player_ico, (width / 8 + width / 2, height / 5),
+                        player_ico)
+            font = ImageFont.truetype('times.ttf', 100)
+            text = 'Gunplay!'
+            draw.text((int(width / 2.3), height / 3),
+                      text, fill='black', font=font)
+            patron_font = ImageFont.truetype('times.ttf', 70)
+            draw.text((width / 4, height / 4 * 2.8),
+                      str(jury_state.bullets[0]), fill='black',
+                      font=patron_font)
+            draw.text((width / 4 + width / 2, height / 4 * 2.8),
+                      str(jury_state.bullets[1]), fill='black',
+                      font=patron_font)
+            patron_ico = image_resize('images/patron-90.png', 80)
+            image.paste(patron_ico, (int(width / 3.2), int(height / 4 * 2.8)),
+                        patron_ico)
+            image.paste(patron_ico, (int(width / 3.2) + width / 2,
+                        int(height / 4 * 2.8)),
+                        patron_ico)
+
+            if jury_state.bullets[0] == jury_state.bullets[1]:
+                text = 'Draw!'
+            elif jury_state.bullets[0] > jury_state.bullets[1]:
+                text = '{} win!'.format(self.players[0].author_name)
+            else:
+                text = '{} win!'.format(self.players[1].author_name)
+            draw.text((width / 3, height / 20),
+                      text, fill='black', font=font)
 
         '''
         Finish drawing field
         '''
-
-        #image.save("test-1.png", "PNG") #if you want to see picture
+        del draw
+        #image.save("test-1.png", "PNG")
         #image.show()
         bytes = BytesIO()
         image.save(bytes, format='png')
         return bytes.getvalue()
 
 '''
-painter = Painter()
-side = 7
+painter = Painter([Player(None, 'Dima'), Player(None, 'Vasya')], fight=True)
+side = 10
 field = [[0 for i in range(side)] for j in range(side)]
-field[0][2] = -2
-field[0][3] = -2
-field[0][4] = -2
-field[1][3] = -1
-field[2][1] = 1
-field[4][3] = 2
-field[3][2] = 3
-field[0][0] = 4
-field[4][1] = 5
-field[2][3] = 6
-jury_state = JuryState(side, field, None, None,
-                       [Player(None, 'Dima'), Player(None, 'Vasya'),
-                        Player(None, 'Alice'), Player(None, 'Nick'),
-                        Player(None, 'Petr'), Player(None, 'Artur')]
-                       )
-painter.paint(jury_state)'''
+field[7][3] = 1
+field[7][4] = 2
+field[8][8] = field[5][3] = field[3][7] = field[3][4] = field[6][6] = -1
+jury_state = JuryState(side, field, [30, 17], None, None)
+painter.paint(jury_state)
+'''
