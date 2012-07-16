a27aaad938b0c62e22ffff6ea9632fe53f03a4fe
Dmitry Tomp <dima_tomp@inbox.ru>
2012-07-16 13:44:05 +0400

    Improved argument processing in create_video.py

--- create_video.py
+++ create_video.py
@@ -5,14 +5,20 @@ from time import clock
 
 if len(sys.argv) < 5:
     print('Usage: game2video.py <path to game env> <folder with game logs> '
-          '<filemask of logs (regular expression)> <resw_name> [<framerate>] '
-          '[--silent]')
+          '<filemask of logs (regular expression)> <resw_name> '
+          '[-f <framerate>] [--silent]')
     exit()
 
 if __name__ == '__main__':
     game_path, log_path, log_mask, res_name = sys.argv[1:5]
-    framerate = 3 if len(sys.argv) < 6 else sys.argv[5]
-    silent = len(sys.argv) > 6 and sys.argv[7] == '--silent'
+    if '-f' not in sys.argv:
+        framerate = 3
+    else:
+        mfpos = 5
+        while sys.argv[mfpos] != '-f':
+            mfpos += 1
+        framerate = str(sys.argv[mfpos + 1])
+    silent = '--silent' in sys.argv
     initialize_game_environment(game_path)
     import config
     beg = clock()
