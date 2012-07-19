42f161e453fad6e9872d36c5e52fab662b9e4265
Ivan Uvarov <totosus@gmail.com>
2012-07-19 14:25:00 +0400

    edited test for the new painter

--- games/pepelac/ascii_painter_test_data.py
+++ games/pepelac/ascii_painter_test_data.py
@@ -10,7 +10,7 @@ tests = [
 [-1,  0,  0, -1,  0,  0],
 [0,  0, -1,  0,  0, -1]
 ],
-'\x1b[41m\x1b[33m\x1b[1m@@@@@@@@@@@@\x1b[0m\n\x1b[42m\x1b[36m\x1b[22m[][]\x1b[45m\x1b[37m\x1b[1mP1\x1b[42m\x1b[36m\x1b[22m[]\x1b[43m\x1b[30m\x1b[22m**\x1b[41m\x1b[33m\x1b[1m@@\x1b[0m\n\x1b[42m\x1b[36m\x1b[22m[]\x1b[43m\x1b[30m\x1b[22m**\x1b[42m\x1b[36m\x1b[22m[][][]\x1b[41m\x1b[33m\x1b[1m@@\x1b[0m\n\x1b[42m\x1b[36m\x1b[22m[][][][][]\x1b[45m\x1b[37m\x1b[1mP2\x1b[0m\n\x1b[43m\x1b[30m\x1b[22m**\x1b[42m\x1b[36m\x1b[22m[][]\x1b[43m\x1b[30m\x1b[22m**\x1b[42m\x1b[36m\x1b[22m[][]\x1b[0m\n\x1b[42m\x1b[36m\x1b[22m[][]\x1b[43m\x1b[30m\x1b[22m**\x1b[42m\x1b[36m\x1b[22m[][]\x1b[43m\x1b[30m\x1b[22m**\x1b[0m\n'
+'\x1b[41m\x1b[33m\x1b[1m@@@@@@@@@@@@\x1b[0m\n\x1b[42m\x1b[36m\x1b[22m[][]\x1b[45m\x1b[37m\x1b[1mP0\x1b[42m\x1b[36m\x1b[22m[]\x1b[43m\x1b[30m\x1b[22m**\x1b[41m\x1b[33m\x1b[1m@@\x1b[0m\n\x1b[42m\x1b[36m\x1b[22m[]\x1b[43m\x1b[30m\x1b[22m**\x1b[42m\x1b[36m\x1b[22m[][][]\x1b[41m\x1b[33m\x1b[1m@@\x1b[0m\n\x1b[42m\x1b[36m\x1b[22m[][][][][]\x1b[45m\x1b[37m\x1b[1mP1\x1b[0m\n\x1b[43m\x1b[30m\x1b[22m**\x1b[42m\x1b[36m\x1b[22m[][]\x1b[43m\x1b[30m\x1b[22m**\x1b[42m\x1b[36m\x1b[22m[][]\x1b[0m\n\x1b[42m\x1b[36m\x1b[22m[][]\x1b[43m\x1b[30m\x1b[22m**\x1b[42m\x1b[36m\x1b[22m[][]\x1b[43m\x1b[30m\x1b[22m**\x1b[0m\n\n\x1b[44m\x1b[33mPlayers in game:\x1b[0m\n\n\x1b[40m\x1b[32mbot1      \x1b[33m by \x1b[36mauthor1        \x1b[33m: \x1b[35m 6 \x1b[33mbullets\x1b[0m\n\x1b[40m\x1b[32mbot2      \x1b[33m by \x1b[36mauthor2        \x1b[33m: \x1b[35m 4 \x1b[33mbullets\x1b[0m\n'
 )
 
 ]
--- games/pepelac/test_ascii_painter.py
+++ games/pepelac/test_ascii_painter.py
@@ -7,11 +7,21 @@ from unittest.mock import Mock
 
 class ascii_painter_test(unittest.TestCase):
     def test_output(self):
-        test_subject = Painter()
+        p1=Mock()
+        p1.bot_name='bot1'
+        p1.author_name='author1'
+        p2=Mock()
+        p2.bot_name='bot2'
+        p2.author_name='author2'
+
+        test_subject = Painter([p1,p2])
         mock_js = Mock()
+        mock_js.bullets=[6,4]
         for test in ascii_painter_test_data.tests:
             mock_js.field = test[0]
             picture = test_subject.ascii_paint(mock_js)
+            print(picture)
+            print(repr(picture))
             self.assertEqual(test[1], picture)
 
 if __name__ == '__main__':
