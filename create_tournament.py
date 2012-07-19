df67b42515ff7ceb4657b726a60637de66c9b759
Andrey Zaytsev <andzaytsev@gmail.com>
2012-07-19 12:03:08 +0400

    Players config has executables, players_files is just files

--- create_tournament.py
+++ create_tournament.py
@@ -1,4 +1,5 @@
 import bot_compiler
+import shutil
 
 def main():
     '''
@@ -12,6 +13,7 @@ def main():
         bots.append((bot[1], bot[3], bot[5]))
         bot = players_config.readline().split('"')
     compiler.compile(bots)
+    shutil.copyfile('config.ini', 'games/pepelac/players_config')
 
 if __name__ == "__main__":
     main()
