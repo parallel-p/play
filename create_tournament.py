720155bfa1cd87542dff475fdcdf657bf9b51d74
Andrey Zaytsev <andzaytsev@gmail.com>
2012-07-19 11:05:53 +0400

    Added create_tournament

---
+++ create_tournament.py
@@ -0,0 +1,17 @@
+import bot_compiler
+
+def main():
+    '''
+    Creates tournament from all the bots that listed in players_config
+    '''
+    compiler = bot_compiler.BotCompiler()
+    players_config = open('games/pepelac/players_config', 'r')
+    bot = players_config.readline().split('"')
+    bots = []
+    while bot != ['']:
+        bots.append((bot[1], bot[3], bot[5]))
+        bot = players_config.readline().split('"')
+    compiler.compile(bots)
+
+if __name__ == "__main__":
+    main()
