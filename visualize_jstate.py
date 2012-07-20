94d00be722c7368ca628790680340cfee8bbbcd9
Timofey Kukushkin <yuri.pechatnov@gmail.com>
2012-07-20 09:31:17 +0400

    more cultural visualize_jstate

--- visualize_jstate.py
+++ visualize_jstate.py
@@ -1,34 +1,44 @@
 import sys
 import os
 import subprocess
+import argparse
 
-if len(sys.argv) < 4:
-    print('''visualize_jstate.py takes 4 arguments:
-             game's path, framerate of video,
-             directory with GameController files,
-             file mask of GameController files (regular expression)''')
-    exit()
+arg_parser = argparse.ArgumentParser(description='')
+arg_parser.add_argument(
+    '-gp', '--game_path', required=True,
+    help='Directory containing game'
+    )
+arg_parser.add_argument(
+    '-f', '--framerate', type=int, required=True,
+    help='Framerate of output video'
+    )
+arg_parser.add_argument(
+    '-d', '--game_controller_directory', required=True,
+    help='Directory with GameController files'
+    )
+arg_parser.add_argument(
+    '-fm', '--game_controller_filemask', required=True,
+    help='File mask of GameController files (regular expression)'
+    )
+args = arg_parser.parse_args()
 
 from visualizer import VideoVisualizer
 import config_helpers
-config_helpers.initialize_game_environment(sys.argv[1])
+config_helpers.initialize_game_environment(args.game_path)
 import config
 
 output_name = 'output.mpg'
-framerate = int(sys.argv[2])
-log_dir_name = sys.argv[3]
-log_files_mask = sys.argv[4]
 
 
 '''
 Run Visualizer
 '''
-vjs = VideoVisualizer(framerate, config.Painter(), log_files_mask, log_dir_name)
+vjs = VideoVisualizer(args.framerate, config.Painter(), args.game_controller_filemask, args.game_controller_directory)
 #vjs = VideoVisualizer(25, config.Painter(), '0-0-0\.jstate', 'logs/tournament1')
 vjs.compile(output_name)
 
 output_name = output_name[:output_name.rfind('.')] + '1.mpg'
-output_place = os.path.join(os.path.dirname(__file__), log_dir_name, output_name)
+output_place = os.path.join(os.path.dirname(__file__), args.game_controller_directory, output_name)
 
 '''
 Play results
