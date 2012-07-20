f4a09e592d5bb9160e18f2bbd58a1899088631a5
Ivan Uvarov <totosus@gmail.com>
2012-07-20 13:59:35 +0400

    score now is 0 if the player was killed before the first move

--- games/pepelac/ascii_painter.py
+++ games/pepelac/ascii_painter.py
@@ -57,9 +57,11 @@ class Painter():
             headercolor=set_color((1, 8, 0)),
             reset=set_color((None, None, 3)))
         for pnum, (player, bulletn) in enumerate(zip(players, bullets)):
-            (bgcolor, endmsg) = ((5, None, None), ' is dead, with score {numcolor}{score:4}{textc}') if player in dead else ((0, None, None), ' has {numcolor}{bullets:4d}{textc} bullets        ')
+            (bgcolor, endmsg) = ((5, None, None), ' is dead, with score {numcolor}{score:4}{textc}' ) if player in dead else ((0, None, None), ' has {numcolor}{bullets:4d}{textc} bullets        ')
+            scr=scores.get(player)
+            scr = 0 if scr is None
             statstr += ('{bkgnd}{icolor}[{player_index}]{botcolor}{player.bot_name:10s}{textc} by {authorcolor}{player.author_name:15s}{textc}' + endmsg + '{reset}\n').format(
-                score=scores.get(player),
+                score=scr,
                 bullets=bulletn,
                 player=player,
                 player_index=self.chars[3].format(pnum),
