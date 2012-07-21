254681693ffe9fd1fcd05ca51a7b283f5cb54a96
Dmitriev Aleksei <denshion@mail.ru>
2012-07-21 09:47:53 +0400

    Added one more bot. It's not standing or random, but not too complicated.

---
+++ games/pepelac/bots/kills_as_soon_as_can_bot.py
@@ -0,0 +1,124 @@
+import sys
+
+
+
+def print_dbg(dbgstring):
+    f = open('/home/denshion/dmitesttxt', mode='ta')
+    f.write(dbgstring + '\n')
+    f.close()
+
+
+def get_distance(my_x, my_y, dest_x, dest_y):
+    return abs(my_x - dest_x) + abs(my_y - dest_y)
+
+
+def make_move(my_x, my_y, dest_x, dest_y):
+    print_dbg('Movement invokation: from ({}; {}) to ({}; {})'.format(my_x, my_y, dest_x, dest_y))
+    print_dbg('Determine: dx={}; dy={}'.format(my_x - dest_x, my_y - dest_y))
+    if abs(dest_x - my_x) < abs(dest_y - my_y):
+        print_dbg('Selected mode |dx| < |dy|')
+        if my_y > dest_y:
+            print_dbg('my_y > dest_y')
+#            print('UP')
+            print('LEFT')
+            print_dbg('Reply: UP')
+        elif dest_y > my_y:
+            print_dbg('my_y < dest_y')
+ #           print('DOWN')
+            print('RIGHT')
+            print_dbg('Reply: DOWN')
+    else:
+        print_dbg('Selected mode |dx| >= |dy|')
+        if my_x > dest_x:
+            print_dbg('my_x > dest_x')
+            print('UP')
+  #          print('LEFT')
+            print_dbg('Reply: LEFT')
+        elif dest_x > my_x:
+            print_dbg('my_x < dest_x')
+            print('DOWN')
+   #         print('RIGHT')
+            print_dbg('Reply: RIGHT')
+        else:
+            print_dbg('DEFAULT CASE')
+            print_dbg('Reply: STAND')
+            print('STAND')
+    sys.stdout.flush()
+
+
+def determine_target(my_x, my_y, targets):
+    result = targets[0]
+    for target in targets:
+        if len(targets[0]) == 3 and (targets[0][2] > target[2] or (
+                targets[0][2] == target[2] and get_distance(
+                    my_x, my_y, target[0], target[1]) < get_distance(
+                        my_x, my_y, result[0], result[1]))):
+            print_dbg('Selected target enemy={}'.format(target))
+            result = target
+        elif len(targets[0]) == 2 and get_distance(
+                my_x, my_y, target[0], target[1]) < get_distance(
+                    my_x, my_y, result[0], result[1]):
+            result = target
+            print_dbg('Selected target bullet={}'.format(target))
+    return result
+
+def main():
+    ''' Strategy:
+        if there are some bullets, gather them;
+            but if we have more bullets, than the enemy does and
+            we can reach him before he finds one, go and kill him.
+        if there are no bullets, do the following:
+            if we have more bullets, than the enemy does, go and kill him.
+            Otherwise, run away (place?) '''
+    n = int(input())
+    while True:
+        input1 = input()
+        input2 = input()
+        (players, bullet_num, k) = map(int, input1.split())
+        (my_x, my_y, my_bullets) = map(int, input2.split())
+        enemies = []
+        for i in range(players - 1):
+            inputt = input()
+            enemy = tuple(map(int, inputt.split()))
+            enemies.append(enemy)
+        bullets = []
+        for i in range(bullet_num):
+            inputt = input()
+            bullet = tuple(map(int, inputt.split()))
+            bullets.append(bullet)
+        print_dbg("bULLETS: " + str(bullet_num))
+        if bullet_num > 0:
+            for enemy in enemies:
+                enemy_bullet = determine_target(
+                    enemy[0], enemy[1], bullets)
+                if enemy[2] < my_bullets and get_distance(
+                        enemy[0], enemy[1],
+                        enemy_bullet[0],
+                        enemy_bullet[1]) > get_distance(
+                            my_x, my_y, enemy[0], enemy[1]):
+                                # If I find an enemy, whose ammo is lower,
+                                # than mine and I would reach him before he
+                                # gets a bullet, I'll go and kill him.
+                                print_dbg('I WILL BREAK YOU!')
+                                make_move(my_x, my_y, enemy[0], enemy[1])
+                                continue
+            # And if there isn't such one, I'll just collect bullets.
+            bullet = determine_target(my_x, my_y, bullets)
+            make_move(my_x, my_y, bullet[0], bullet[1])
+        else:
+            print_dbg("Enemies: " + str(len(enemies)))
+            if len(enemies) == 0:
+                make_move(my_x, my_y, n // 2, n // 2)
+                # If I'm alone, I'll go to the center.
+            else:
+                # When there are no bullets left, if I have less ammo, than
+                # enemies do, I will lose no matter what I do. So, a suicide
+                # attack is better here.
+                target = determine_target(my_x, my_y, enemies)
+                make_move(my_x, my_y, target[0], target[1])
+#        (players, bullet_num, k) = map(int, input().split())
+
+
+if __name__ == '__main__':
+    main()
+
