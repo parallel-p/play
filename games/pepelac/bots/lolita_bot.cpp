7bc63900a5ff2119e374ec713fb8131c6ebfd2f8
Andrey Putilin <regresscheck@gmail.com>
2012-07-21 12:54:23 +0400

    Fixed lolita_bot

--- games/pepelac/bots/lolita_bot.cpp
+++ games/pepelac/bots/lolita_bot.cpp
@@ -19,12 +19,14 @@ const int dy[4] = {0, 1, 0, -1};
 
 int n, p, b, k, x = 1, y = 1, bx, by, t;
 int c[100][100];
-bool used[100][100], made_move;
+bool used[100][100], made_move, buls[100][100];
 player players[50];
-bullet bullets[100000];
+bullet bullets[10000];
 
 bool comp_bullets(bullet a, bullet b)
 {
+    if (a.mn != b.mn)
+        return a.mn < b.mn;
     return a.g > b.g;
 }
 
@@ -38,6 +40,24 @@ int sign(int x)
     if (x < 0)
         return -1;
     return (x > 0);
+}
+
+void random_move()
+{
+    if (good_move(x + 1, y))
+        printf("RIGHT\n");
+    else
+        if (good_move(x - 1, y))
+            printf("LEFT\n");
+        else
+            if (good_move(x, y + 1))
+                printf("DOWN\n");
+            else
+                if (good_move(x, y - 1))
+                    printf("UP\n");
+                else
+                    printf("STAND\n");
+    fflush(stdout);
 }
 
 int main()
@@ -63,33 +83,34 @@ int main()
     {
         for (int i = 1; i <= n; i++)
             for (int j = 1; j <= n; j++)
+            {
                 used[i][j] = false;
+                buls[i][j] = false;
+            }
         scanf("%d%d%d", &p, &b, &k);
         for (int i = 0; i < p; i++)
         {
             scanf("%d%d%d", &players[i].y, &players[i].x, &players[i].p);
-            used[players[i].y][players[i].x] = true;
         }
         for (int i = 0; i < b; i++)
+        {
             scanf("%d%d", &bullets[i].y, &bullets[i].x);
+            buls[bullets[i].y][bullets[i].x] = true;
+        }
+        for (int i = 1; i < p; i++)
+        {
+            used[players[i].y][players[i].x] = true;
+            for (int j = 0; j < 4; j++)
+                if (players[i].y + dy[j] >= 1 && players[i].y + dy[j] <= n &&
+                    players[i].x + dx[j] >= 1 && players[i].x + dx[j] <= n &&
+                    players[i].p > players[0].p + buls[players[i].y + dy[j]][players[i].x + dx[j]])
+                    used[players[i].y + dy[j]][players[i].x + dx[j]] = true;
+        }
         x = players[0].x;
         y = players[0].y;
         if (p == 1)
         {
-            if (good_move(x + 1, y))
-                printf("RIGHT\n");
-            else
-                if (good_move(x - 1, y))
-                    printf("LEFT\n");
-                else
-                    if (good_move(x, y + 1))
-                        printf("DOWN\n");
-                    else
-                        if (good_move(x, y - 1))
-                            printf("UP\n");
-                        else
-                            printf("STAND\n");
-            fflush(stdout);
+            random_move();
             continue;
         }
         for (int i = 0; i < b; i++)
@@ -108,9 +129,11 @@ int main()
         made_move = false;
         for (int i = 0; i < b && !made_move; i++)
         {
-            if (bullets[i].g >= 0 && c[bullets[i].x][bullets[i].y] < t - bullets[i].dist[0])
+            if (((p == 2 && players[1].p < players[0].p && bullets[i].g >= -2) ||
+                 (p == 2 && players[1].p == players[0].p && bullets[i].g >= 0) ||
+                 bullets[i].g >= 2) && c[bullets[i].y][bullets[i].x] < t - bullets[i].dist[0])
             {
-                if (c[y][x + sign(bullets[i].x - x)] < t - 1 && bullets[i].x - x != 0 && !used[y][x + sign(bullets[i].x - x)])
+                if (c[y][x + sign(bullets[i].x - x)] < t - 1 && bullets[i].x - x != 0 && !used[y][x + sign(bullets[i].x - x)] && (!buls[y + sign(bullets[i].y - y)][x] || !(c[y + sign(bullets[i].y - y)][x] < t - 1 && bullets[i].y - y != 0 && !used[y + sign(bullets[i].y - y)][x])))
                 {
                     if (bullets[i].x - x > 0)
                         printf("RIGHT\n");
@@ -134,7 +157,7 @@ int main()
         }
         if (!made_move)
         {
-            if (bx - x != 0 && !used[y][x + sign(bx - x)])
+            if (bx - x != 0 && !used[y][x + sign(bx - x)] && c[y][x + sign(bx - x)] < k - 1)
             {
                 if (bx - x > 0)
                     printf("RIGHT\n");
@@ -143,7 +166,7 @@ int main()
                 fflush(stdout);
                 continue;
             }
-            if (by - y != 0 && !used[y + sign(by - y)][x])
+            if (by - y != 0 && !used[y + sign(by - y)][x] && c[y + sign(by - y)][x] < k - 1)
             {
                 if (by - y > 0)
                     printf("DOWN\n");
@@ -152,8 +175,13 @@ int main()
                 fflush(stdout);
                 continue;
             }
-            printf("STAND\n");
-            fflush(stdout);
+            if (x == bx && y == by && p >= 2 && players[1].p > players[0].p)
+                random_move();
+            else
+            {
+                printf("STAND\n");
+                fflush(stdout);
+            }
         }
     }
     return 0;
