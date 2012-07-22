3a2d128a80f3cc310f659f09dbf470702b1350a4
Arthur Khashaev <arthur@khashaev.ru>
2012-07-22 12:52:26 +0400

    Removed redundant ascii_paint method from painters

--- games/tic-tac-toe/painter.py
+++ games/tic-tac-toe/painter.py
@@ -17,10 +17,3 @@ class Painter:
 
     def paint(self, jury_state):
         pass
-
-    def ascii_paint(self, js):
-        ''' This method is used to paint in ASCII-art for using with
-        ASCII visualizer (from development tools). It returns an ASCII
-        string with image of the game field. '''
-        return '\n'.join([' '.join([j \
-            for j in js.field[i * 3:(i + 1) * 3]]) for i in range(3)])
