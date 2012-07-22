62506fc851835cc826cecef508ce7eb96837d1e2
Dmitriev Aleksei <denshion@mail.ru>
2012-07-22 12:26:37 +0400

    Fixed imports in move_tests in pepelac

--- games/pepelac/move_tests.py
+++ games/pepelac/move_tests.py
@@ -1,19 +1,22 @@
 import unittest
 import io
-from move import deserialize, DeserializeMoveException
+from move import deserialize_move, DeserializeMoveException
 
 
 class MoveTests(unittest.TestCase):
     def test_deserialization(self):
-        stream = io.BytesIO(b'Stand\nDown\nNothing\n')
-        move = deserialize(stream)
+        stream = io.BytesIO(b'STAND\nDOWN\n')
+        move = deserialize_move(stream)
         self.assertEqual(move[0], 0)
         self.assertEqual(move[1], 0)
-        move = deserialize(stream)
+        move = deserialize_move(stream)
         self.assertEqual(move[0], 1)
         self.assertEqual(move[1], 0)
+
+    def test_error_raising(self):
+        stream = io.BytesIO(b'SOMETHING--Odd')
         with self.assertRaises(DeserializeMoveException):
-            move = deserialize(stream)
+            move = deserialize_move(stream)
 
 
 if __name__ == '__main__':
