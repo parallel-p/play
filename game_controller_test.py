a79ef68bbfc377d5c88236d2f66f4d9ca6a8ec45
Vadim Drobinin <vadim@drobinin.com>
2012-07-22 10:25:11 +0400

    Rewritten game_controller_test.py

--- game_controller_test.py
+++ game_controller_test.py
@@ -1,43 +1,38 @@
+GAME_PATH = 'games/pepelac'
+import config_helpers
+config_helpers.initialize_game_environment(GAME_PATH)
+import config
 import unittest
 import copy
 import bot
 from unittest.mock import Mock
 import game_controller
-
+from game_controller import GameNotFinishedException
 
 class GameControllerTest(unittest.TestCase):
     def setUp(self):
-        self.num_players = 5
-        self.config = 'cfg'
-        player = Mock()
-        player.command_line = 'bash'
-        self.players = [copy.deepcopy(player) for i in range(self.num_players)]
+        self.players = [4, 5]
         self.signature = ''
         self.jury_state = ''
-        self.game = game_controller.GameController(self.players, self.signature,
-                                                   self.jury_state)
+        self._simulator = Mock()
+        self.game = game_controller.GameController(self.players,\
+                    self.signature, self.jury_state, self._simulator)
+        self.game.is_finished = False
+        self.game._scores = {'5'}
+    
+    def test_finish_game(self):
+        self.is_finished = False
+        with self.assertRaises(GameNotFinishedException):
+            self.game.get_scores()
+        self.game.finish_game(self.game._scores)
+        self.assertTrue(self.game.is_finished)
 
-    def test_create_bots(self):
-        bot.Bot = Mock()
-        bot.Bot.return_value = True
-        self.game.create_bots()
-        self.assertEqual(self.game.bots, dict(zip(self.players,
-            [True] * self.num_players)))
-        self.assertEqual(bot.Bot.call_args_list,
-            [(("bash", "cfg"),)] * self.num_players)
-
-    def test_kill_bots(self):
-        self.game.kill_bots()
-        self.assertEqual(len(self.game.bots), 0)
+    def test_get_scores(self):
+        self.game.is_finished = True
+        self.assertEqual(self.game._scores, self.game.get_scores())
 
     def test_get_players(self):
-        self.assertEqual(self.game.get_players(), self.players)
-
-    def test_report_state(self):
-        begin = len(self.game.jury_states)
-        self.game.report_state(self.jury_state)
-        end = len(self.game.jury_states)
-        self.assertEqual(begin + 1, end)
+        self.assertEqual(self.players, self.game.get_players())
 
 if __name__ == '__main__':
     unittest.main()
