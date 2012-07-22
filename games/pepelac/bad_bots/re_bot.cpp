af1eb527e848e1994fc4809d85861e072552ade1
Aganov Artur <artur@Aganov.nout>
2012-07-22 10:09:21 +0400

    added Runtime Error bot

---
+++ games/pepelac/bad_bots/re_bot.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <stdlib.h>
+#include <set>
+#include <map>
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+
+using namespace std;
+
+int main(){
+  //freopen("input.txt", "r", stdin);
+  //freopen("outputtxt", "w", stdout);
+  cout << "STAND" << endl;
+  cout.flush();
+
+  return 123;
+}
