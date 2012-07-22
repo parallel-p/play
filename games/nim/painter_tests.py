cd87ed8d15c58f910ae71035f378560d8f11001e
Dmitriev Aleksei <denshion@mail.ru>
2012-07-22 10:46:32 +0400

    Fixed painter_tests in nim

--- games/nim/painter_tests.py
+++ games/nim/painter_tests.py
@@ -8,9 +8,9 @@ import random
 class PainterTests(unittest.TestCase):
     def test_type(self):
         jury_state = JuryState(list(range(20)))
-        current_painter = Painter()
+        current_painter = Painter(['John Smith'])
         byte_string = current_painter.paint(jury_state)
-        self.assertTrue(isinstance(byte_string, str))
+        self.assertTrue(isinstance(byte_string, bytes))
 
 if __name__ == '__main__':
     unittest.main()
