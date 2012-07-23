4f7a76ebe0392645be96b6971e1ce63ebc228d14
Timofey Kukushkin <yuri.pechatnov@gmail.com>
2012-07-23 13:45:00 +0400

    Exclusion testfiles from Coverage

--- run_tests_with_coverage.py
+++ run_tests_with_coverage.py
@@ -1,26 +1,45 @@
 #!/usr/bin/env python
 import sys
+import os
+import re
 import unittest
 from coverage import coverage as Coverage
 from log import logger
+import argparse
 
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(description="""
+        This script runs all test and show coverage with Coverage""")
+    arg_parser.add_argument(
+        '-hr', '--html-report',
+        help='Directory save htmlreport to'
+        )
+    args = arg_parser.parse_args();
 
 def main():
-    coverage = Coverage()
+    list_of_test_files = list()
+    for directory in os.walk('.'):
+        for filename in directory[2]:
+            if re.search(".*_tests?\.py", filename):
+                list_of_test_files.append(filename)
+    coverage = Coverage(omit=list_of_test_files)
     coverage.start()
 
     # no log messages
     logger.setLevel(1000000000000)
 
     loader = unittest.TestLoader()
-    suite = loader.discover(".", "*_test*.py")
+    suite = loader.discover('.', '*_test.py')
     test_runner = unittest.TextTestRunner(verbosity=2)
     test_result = test_runner.run(suite)
 
     coverage.stop()
     coverage.report(file=sys.stdout)
 
+    if args.html_report:
+        coverage.html_report(directory=args.html_report)#, ignore_errors=True)
     print(test_result)
+
 
 if __name__ == '__main__':
     main()
