6864488cfe3134760ee258eb043461fb4a84b114
Andrey Zaytsev <andzaytsev@gmail.com>
2012-07-23 13:58:47 +0400

    Fixed series tests

--- tournament_stages/series_test.py
+++ tournament_stages/series_test.py
@@ -3,36 +3,32 @@ sys.path.append('/home/daniel/play/play/')
 
 import unittest
 from unittest.mock import Mock, patch
-from tournament_stages.game_signature import GameSignature
 from log import logger
-with patch.dict('sys.modules', {'config': Mock()}):
-    import game
-    from tournament_stages.series import Series
+from tournament_stages.series import Series
 
 
 class SeriesTest(unittest.TestCase):
 
     def test_series_init(self):
-        signature = GameSignature(1, 1, 1, 1)
+        signature = Mock()
         series1 = Series([1], signature, [1, 2])
         self.assertEqual(series1._initial_jurystates, [1])
         self.assertEqual(series1._signature, signature)
 
-    def test_run(self):
+    @patch('tournament_stages.series.Game')
+    def test_run(self, mock_class):
         logger.setLevel(10050000)
-        inst = Mock()
-        inst.get_results.return_value = {
+        signature = Mock()
+        mock_class().get_results.return_value = {
             '1': 123,
         }
-        signature = GameSignature(1, 1, 1, 1)
-        game.Game.return_value = inst
         series1 = Series([1], signature, [1, 2])
         series1.run()
         result = {signature: {'1': 123}}
-        self.assertEqual(series1._results, result)
+        self.assertEqual(list(series1._results.values()), list(result.values()))
 
     def test_get_results(self):
-        signature = GameSignature(1, 1, 1, 1)
+        signature = Mock()
         series1 = Series([1], signature, [1, 2])
         self.assertRaises(Exception, series1.get_results)
         series1._results = '123'
