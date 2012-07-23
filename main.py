fa873724d3381949366ea45dc1eb67a84172c29f
Timofey Kukushkin <yuri.pechatnov@gmail.com>
2012-07-23 11:10:16 +0400

    main and main_test fixes

--- main.py
+++ main.py
@@ -4,26 +4,20 @@ import argparse
 from exceptions import PlayersException
 
 '''
-These strings add project_root to sys.path
-'''
-project_root = os.path.normpath(os.path.join(os.path.dirname(__file__)))
-if project_root not in sys.path:
-    sys.path.insert(0, project_root)
-
-
-def get_free_dirname(path, filename_begin):
-    for i in range(1, 1024):
-        name = os.path.normpath(os.path.join(path, filename_begin + str(i)))
-        if not os.path.exists(name):
-            return i
-
-
-'''
 This horrible code parse arguments and import config and config_helpers,
 with function initialize_game_environment it add game_path to sys.path
 '''
 import config_helpers
 if __name__ == '__main__':
+    '''
+    These strings add project_root to sys.path
+    '''
+    project_root = os.path.normpath(os.path.join(os.path.dirname(__file__)))
+    if project_root not in sys.path:
+        sys.path.insert(0, project_root)
+    '''
+    Argument parseing:
+    '''
     arg_parser = argparse.ArgumentParser(description='''
 Framework runs game, placed in "game_path".
 Players in this tournament read from file
@@ -41,26 +35,24 @@ Tournament id which is used for saving logs,
 if you don't set tournament id it will be least non-used one'''
     )
     args = arg_parser.parse_args()
-    game_path = args.game_path
     '''
     If tournament id isn't setted it will be least non-used one
     '''
-    if not args.tournament_id:
-        args.tournament_id = get_free_dirname(path='logs/',
-                                              filename_begin='tournament')
-    config_helpers.initialize_game_environment(game_path)
+    
+    config_helpers.initialize_game_environment(args.game_path)
 import config
 
 from tournament_stages.tournament import Tournament
 from utils import print_tournament_system_results
-
+import bot
 
 class Main:
 
     def __init__(self, game_path, tournament_id):
         self._game_path = game_path
-        self.players_list = None
-        self.tournament_id = tournament_id
+        self._players_list = None
+        self._tournament_id = tournament_id
+        self.tournament = None
 
     def _load_players(self):
         '''
@@ -69,16 +61,16 @@ class Main:
         '''
         players_config_path = os.path.join(self._game_path,
                                            config.players_config)
-        self.players_list = config_helpers.players_parse(players_config_path)
-        if self.players_list is None:
+        self._players_list = config_helpers.players_parse(players_config_path)
+        if self._players_list is None:
             raise PlayersException('Players list doesn\'t exist')
 
     def _run_tournament(self):
         '''
         Run tournament and get it's results
         '''
-        self.tournament = Tournament(self.players_list,
-                                     self.tournament_id)
+        self.tournament = Tournament(self._players_list,
+                                     self._tournament_id)
         self.tournament.run()
         self.tournament_results = self.tournament.get_results()
 
@@ -86,11 +78,22 @@ class Main:
         return self.tournament_results
 
     def _print_tournament_results(self, ts):
-            print_tournament_system_results(ts)
+        print_tournament_system_results(ts)
 
+    def _get_free_dirname(self, path, dirname_begin):
+        for i in range(1, 1024):
+            name = os.path.normpath(os.path.join(path, dirname_begin + str(i)))
+            if not os.path.exists(name):
+                return i
+
+    def _make_good_tournament_id(self):
+        if not self._tournament_id:
+            self._tournament_id = self._get_free_dirname(path='logs/',
+                dirname_begin='tournament')
+        
     def main(self):
         self._load_players()
-        import bot
+        self._make_good_tournament_id()
         try:
             self._run_tournament()
             self._print_tournament_results(self.tournament.tournament_system)
@@ -98,5 +101,5 @@ class Main:
             pass
 
 if __name__ == '__main__':
-    main = Main(game_path, args.tournament_id)
+    main = Main(args.game_path, args.tournament_id)
     main.main()
--- main_test.py
+++ main_test.py
@@ -1,5 +1,7 @@
+import os
 import unittest
 from unittest.mock import MagicMock, patch
+from exceptions import PlayersException
 
 with patch.dict('sys.modules',
                 {'config_helpers': MagicMock(),
@@ -8,31 +10,74 @@ with patch.dict('sys.modules',
                  'Tournament': MagicMock(),
                  '_load_players': MagicMock(),
                  'print_tournament_system_results': MagicMock()}):
-    import config
-    import config_helpers
-    from tournament_stages.tournament import Tournament
-    from utils import print_tournament_system_results
     import main
-
+    
 
 class MainTest(unittest.TestCase):
 
-    def test_functions(self):
-        self.main = main.Main('Ata dir', 1)
-        #self.main._load_players()
-        Tournament().get_results.return_value = 'First player won'
+    def setUp(self):
+        self.main = main.Main('game_path', 42)
+        main.config_helpers = MagicMock()
+        main.config = MagicMock()
+        main.Tournament = MagicMock()
+
+    def test_init(self):
+        self.assertEqual(self.main._game_path, 'game_path')
+        self.assertEqual(self.main._tournament_id, 42)
+
+    def test_load_players(self):
+        main.config_helpers.players_parse.return_value = [1, 2, 3]
+        main.config.players_config = '42'
+        self.main._load_players()
+        self.assertEqual(self.main._players_list, [1, 2, 3])
+        main.config_helpers.players_parse.return_value = None
+        try:
+            self.main._load_players()
+        except PlayersException:
+            return
+        else:
+            raise Exception('Test of _load_players failed')
+
+    def test_run_tournament_and_show_result(self):
+        main.Tournament().get_results.return_value = 42
+        main.Tournament().run = MagicMock()
         self.main._run_tournament()
-        self.main._print_tournament_results(MagicMock())
-        #assertNotEqual(self.main.show_result(), 'First player won')
-        if self.main.show_result() != 'First player won':
-            raise Exception("main.py doesn't work")
-    '''
+        self.assertEqual(self.main.tournament_results, 42)
+        self.assertEqual(self.main.show_result(), 42)
+        main.Tournament().run.assert_called_once_with()
+    
+    def test_print_tournament_system_results(self):
+        main.print_tournament_system_results = MagicMock()
+        self.main._print_tournament_results(42)
+        main.print_tournament_system_results.assert_called_once_with(42)
+
+    def test_get_free_dirname(self):
+        if self.main._get_free_dirname('logs', 
+                                       'tournament') not in range(1, 1024):
+            raise Exception('_get_free_dirname doesn\'t work')
+
+    def test_make_good_tournament_id(self):
+        self.main._tournament_id = None
+        self.main._make_good_tournament_id()
+        self.assertNotEqual(self.main._tournament_id, None)
+        self.main._tournament_id = 42
+        self.main._make_good_tournament_id()
+        self.assertEqual(self.main._tournament_id, 42)
+
     def test_main(self):
-        self.main = main.Main('Ata dir', 1)
-        Tournament().get_results.return_value = 'First player won'
+        main.bot = MagicMock()
+        self.main._load_players = MagicMock()
+        self.main._make_good_tournament_id = MagicMock()
+        self.main._run_tournament = MagicMock()
+        self.main._print_tournament_results = MagicMock()
+        self.main.tournament = MagicMock()
+        self.main.tournament.tournament_system = MagicMock()
         self.main.main()
-        if self.main.show_result() != 'First player won':
-            raise Exception("main.py doesn't work")'''
+        self.main._load_players.assert_called_once_with()
+        self.main._make_good_tournament_id.assert_called_once_with()
+        self.main._run_tournament.assert_called_once_with()
+        self.main._print_tournament_results.assert_called_once_with(\
+            self.main.tournament.tournament_system)
 
 
 if __name__ == '__main__':
