91520c781f6a06fc838ce2df279e03cc152a2fef
Andreev Alexander <carzil@yandex.ru>
2012-07-23 22:59:20 +0800

    print print('\n') -> \n\n

--- game_controller.py
+++ game_controller.py
@@ -47,7 +47,7 @@ class GameController:
         '''
         self.is_finished = True
         self._scores = scores
-        print('\n')
+        print()
         logger.info('game finished')
 
     def get_scores(self):
