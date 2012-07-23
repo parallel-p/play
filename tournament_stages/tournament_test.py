e4f18c7c4ca15df59ccfe1d6be43f317d1bf6950
Andrey Putilin <regresscheck@gmail.com>
2012-07-23 10:28:20 +0400

    Fixed round unittest

--- tournament_stages/tournament_test.py
+++ tournament_stages/tournament_test.py
@@ -1,22 +1,14 @@
 import unittest
 import sys
 import os
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, patch, Mock
 
-project_root = os.path.normpath(os.path.join(os.path.dirname(__file__), '..'))
-if project_root not in sys.path:
-    sys.path.insert(0, project_root)
+
 with patch.dict('sys.modules',
-                {'tournament_systems.tournament_system': MagicMock(),
-                 'TournamentSystem': MagicMock(),
-                 'tournament_stages.round': MagicMock(),
-                 'Round': MagicMock(),
+                {'tournament_stages.round': MagicMock(),
                  'tournament_systems.tournament_system_factory': MagicMock(),
-                 'create': MagicMock(),
-                 'log': MagicMock(),
-                 'logger': MagicMock()}):
-    from tournament_systems.tournament_system import TournamentSystem
-    from tournament_systems.tournament_system_factory import create
+                 'log': MagicMock()}):
+    import tournament_systems.tournament_system_factory as factory
     from tournament_stages.round import Round
     from log import logger
     import tournament_stages.tournament as tournament_file
@@ -24,15 +16,16 @@ with patch.dict('sys.modules',
 
 class TournamentTest(unittest.TestCase):
     def test_tournament(self):
-        create()().get_results = 5
-        create()().get_rounds.return_value = [{1: {1: 0, 2: 1}},
+        tournament_file.Round = Mock()
+        tournament_file.create = Mock()
+        tournament_file.tournament_system = Mock()
+        tournament_file.create()().get_results = 5
+        tournament_file.create()().get_rounds.return_value = [{1: {1: 0, 2: 1}},
                                                       {2: {2: 0, 3: 1}}]
-        create()().get_all_results.return_value = 'First player won'
+        tournament_file.create()().get_all_results.return_value = 'First player won'
         tournament = tournament_file.Tournament([1, 2, 3], 1)
         tournament.run()
-        result = tournament.get_results()
-        if result != 'First player won':
-            raise Exception("tournament.py or 'patch.dict' doesn't work")
+        self.assertEqual(tournament.get_results(), 'First player won')
 
 if __name__ == '__main__':
     unittest.main()
