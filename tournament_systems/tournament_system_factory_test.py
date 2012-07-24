14a4af7dff9ecb5d5123dc6433971455ffd0bf9f
Andrey Putilin <regresscheck@gmail.com>
2012-07-24 14:59:31 +0400

    Fixed tournament system factory test

--- tournament_systems/tournament_system_factory_test.py
+++ tournament_systems/tournament_system_factory_test.py
@@ -4,8 +4,8 @@ with patch.dict('sys.modules',
                 {'config': Mock(),
                  'tournament_systems_config': Mock()}):
     import config
-    import tournament_systems_config as all_ts
-    import tournament_system_factory as ts_factory
+    import tournament_systems.tournament_systems_config as all_ts
+    import tournament_systems.tournament_system_factory as ts_factory
     #!!!
 
 
@@ -19,20 +19,15 @@ class TS2:
 
 class TournamentSystemFactoryTests(unittest.TestCase):
     def setUp(self):
+        ts_factory.config = Mock()
         all_ts.tournament_systems = {'ts1': TS1, 'ts2': TS2}
 
     def test_all_tournament_systems(self):
         for ts_name, ts in all_ts.tournament_systems.items():
-            config.tournament_system = ts_name
+            ts_factory.config.tournament_system = ts_name
             self.assertTrue(isinstance(
                 ts_factory.create()(), ts
             ))
-
-    def test_exception_not_choosed_tournament_system(self):
-        if hasattr(config,"tournament_system"):
-            del config.tournament_system
-        with self.assertRaises(ts_factory.TournamentSystemNotChoosedException):
-            ts_factory.create()
 
     def test_exception_not_available_tournament_system(self):
         config.tournament_system = 'abacaba'
