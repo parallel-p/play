dbef4337f8eb8044caf2a05bd101e0badd5a2da8
DimaPhil <dmitry239.philippov@mail.ru>
2012-08-10 18:52:22 +0400

    Simple version of random bot

--- games/tic-tac-toe/bots/random_bot.py
+++ games/tic-tac-toe/bots/random_bot.py
@@ -1,17 +1,21 @@
 import sys
 import random
 
-field = [['.' for i in range(3)] for j in range(3)]
+'''field = [['.' for i in range(3)] for j in range(3)]
 for i in range(3):
     row = sys.stdin.readline().split()
     for j, element in enumerate(row):
         field[i][j] = element
 
-random.seed()
-while True:
+random.seed(239)
+for t in range(100):
     i = random.randint(0, 2)
     j = random.randint(0, 2)
     if field[i][j] == '.':
         print(i + 1, j + 1)
         sys.stdout.flush()
         break
+'''
+random.seed()
+print(str(random.randint(1, 3)) + ' ' + str(random.randint(1, 3)))
+sys.stdout.flush()
