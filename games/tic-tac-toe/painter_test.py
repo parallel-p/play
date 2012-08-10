7ab82de47b98a0b12abf0f4694a512f468c92998
DimaPhil <dmitry239.philippov@mail.ru>
2012-08-11 01:26:47 +0400

    Modified test for painter

--- games/tic-tac-toe/painter_test.py
+++ games/tic-tac-toe/painter_test.py
@@ -1,5 +1,6 @@
 from jury_state import JuryState
 from painter import Painter
+from player import Player
 import unittest
 import random
 
@@ -9,7 +10,7 @@ class PainterTests(unittest.TestCase):
         jury_state = JuryState(list(range(9)))
         jury_state.field[2] = 'X'
         jury_state.field[6] = 'O' 
-        current_painter = Painter([None, None])
+        current_painter = Painter([Player('c', 'Petya'), Player('c', 'Dmitry Philippov')])
         byte_string = current_painter.paint(jury_state)
 
 if __name__ == '__main__':
