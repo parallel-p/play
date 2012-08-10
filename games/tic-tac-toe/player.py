7355427a57dd238af96744dabbd505f9ff07bbbf
DimaPhil <dmitry239.philippov@mail.ru>
2012-08-11 02:52:27 +0400

    Added player.py

---
+++ games/tic-tac-toe/player.py
@@ -0,0 +1,23 @@
+class Player:
+    def __init__(self, cmd_line, author='John Doe', bot='Some bot'):
+        self.author_name = author
+        self.bot_name = bot
+        self.command_line = cmd_line
+
+    def __str__(self):
+        return '{}, {}'.format(self.author_name, self.bot_name)
+
+    def __repr__(self):
+        return '<{}>'.format(self.__str__())
+
+    def _as_tuple(self):
+        return (self.author_name, self.bot_name, self.command_line)
+
+    def __lt__(self, other):
+        return self._as_tuple() < other._as_tuple()
+
+    def __eq__(self, other):
+        return self._as_tuple() == other._as_tuple()
+
+    def __hash__(self):
+        return self._as_tuple().__hash__()
