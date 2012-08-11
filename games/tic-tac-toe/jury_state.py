59dfc85efd3d7b3db4f12a9907c77f022e3699cd
DimaPhil <dima239philippov@gmail.com>
2012-08-11 15:37:52 +0400

    Modified jury_state

--- games/tic-tac-toe/jury_state.py
+++ games/tic-tac-toe/jury_state.py
@@ -1,4 +1,11 @@
 class JuryState:
-    def __init__(self, field, winner=None):
+    def __init__(self, field, winner=None, line=-1):
+        '''
+        field - List of cells ('.', 'X', 'O')
+        winnner - Winner of the game (type is Player).
+                  It is equal to None is game is not finished
+        line - line of winner
+        '''
         self.field = field
         self.winner = winner
+        self.line = line
