611be8154dddfbebbcc8e0c4b885c818a9f7cf93
Andreev Alexander <carzil@yandex.ru>
2012-09-04 01:49:00 +0400

    PEP8 fixes in compile_bots.py

--- compile_bots.py
+++ compile_bots.py
@@ -20,8 +20,9 @@ def run_app():
         s = bot.split('\"')
         players.append([s[1], s[3], s[5]])
     compiler.compile(players, os.path.join(game_path, 'players_config'))
-    print('File with commands saved in ' + os.path.join(game_path,
-                                                       'players_config') + '.')
+    print('File with commands saved in {}'.format(
+        os.path.join(game_path, 'players_config') + '.')
+    )
 
 if __name__ == '__main__':
     run_app()
